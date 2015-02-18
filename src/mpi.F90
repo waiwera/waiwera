af72fe2e49785f4d2b67fe46fee79e19f36a6ba1
Adrian Croucher <a.croucher@auckland.ac.nz>
2015-02-18 14:46:01 +1300

    Added mpi module

---
+++ src/mpi.F90
@@ -0,0 +1,15 @@
+module mpi_module
+  !! MPI variables.
+
+  implicit none
+
+  private
+
+#include <petsc-finclude/petscsys.h>
+#include <petsc-finclude/petscdef.h>
+
+  MPI_Comm, public :: comm !! MPI communicator
+  PetscMPIInt, public :: rank !! processor rank
+  PetscMPIInt, public, parameter :: input_rank = 0 !! Processor rank for input handling
+
+end module mpi_module
