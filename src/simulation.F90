e91ee7454f3b8b6106347f67bf348b7a6d602f3f
Adrian Croucher <a.croucher@auckland.ac.nz>
2015-02-25 15:55:57 +1300

    Added simulation%read_initial() and beginnings of simulation%read_timestepping() routines

--- src/simulation.F90
+++ src/simulation.F90
@@ -19,6 +19,7 @@ module simulation_module
 
 #include <petsc-finclude/petscsys.h>
 #include <petsc-finclude/petscdef.h>
+#include <petsc-finclude/petscvec.h>
 
   integer, parameter, public :: max_filename_length = 200
   integer, parameter, public :: max_title_length = 120
@@ -27,6 +28,7 @@ module simulation_module
      !! Simulation type.
      private
      type(mesh_type) :: mesh
+     Vec :: initial
      type(timestepper_type) :: timestepper
      class(thermodynamics_type), pointer :: thermo
      class(eos_type), pointer :: eos
@@ -37,6 +39,8 @@ module simulation_module
      procedure :: read_title => simulation_read_title
      procedure :: read_thermodynamics => simulation_read_thermodynamics
      procedure :: read_eos => simulation_read_eos
+     procedure :: read_initial => simulation_read_initial
+     procedure :: read_timestepping => simulation_read_timestepping
      procedure, public :: init => simulation_init
      procedure, public :: run => simulation_run
      procedure, public :: destroy => simulation_destroy
@@ -117,6 +121,132 @@ contains
 
 !------------------------------------------------------------------------
 
+  subroutine simulation_read_initial(self, json)
+    !! Reads initial conditions from JSON input. These may be specified
+    !! as a constant value or as an array. The array may be of length
+    !! equal to the number of primary variables (to apply the same initial
+    !! conditions in each cell) or as a complete specification of 
+    !! initial conditions for all cells.
+
+    use fson_value_m
+
+    class(simulation_type), intent(in out) :: self
+    type(fson_value), pointer, intent(in) :: json
+    ! Locals:
+    type(fson_value), pointer :: initial
+    PetscErrorCode :: ierr
+    real(dp) :: const_initial_value
+    integer :: int_const_initial_value
+    integer, allocatable :: indices(:)
+    real(dp), allocatable :: initial_input(:), initial_data(:)
+    integer :: i, np, count
+    logical :: const
+    real(dp), parameter :: default_initial_value = 0.0_dp
+
+    call DMCreateGlobalVector(self%mesh%dm, self%initial, ierr); CHKERRQ(ierr)
+    call VecGetSize(self%initial, count, ierr); CHKERRQ(ierr)
+
+    if (mpi%rank == mpi%input_rank) then
+       
+       const = .true.
+       call fson_get(json, "initial", initial)
+       if (associated(initial)) then
+
+           ! if (initial%value_type == TYPE_STRING) then
+              ! interpret as filename and read from separate file,
+              ! then continue as below
+           ! end if
+
+          if (initial%value_type == TYPE_REAL) then
+
+             call fson_get(initial, ".", const_initial_value)
+
+          else if (initial%value_type == TYPE_INTEGER) then
+
+             call fson_get(initial, ".", int_const_initial_value)
+             const_initial_value = real(int_const_initial_value)
+
+          else if (initial%value_type == TYPE_ARRAY) then
+
+             const = .false.
+             call fson_get(initial, ".", initial_input)
+             np = size(initial_input)
+             allocate(initial_data(count))
+             if (np == count) then
+                initial_data = initial_input
+             else
+                ! repeat input over array:
+                do i = 1, np
+                   initial_data(i:count:np) = initial_input(i)
+                end do
+             end if
+             deallocate(initial_input)
+
+          end if
+       else
+          const_initial_value = default_initial_value
+       end if
+    end if
+
+    call MPI_bcast(const, 1, MPI_LOGICAL, mpi%input_rank, mpi%comm, ierr)
+    
+    if (const) then
+
+       call MPI_bcast(const_initial_value, 1, MPI_DOUBLE_PRECISION, &
+            mpi%input_rank, mpi%comm, ierr)
+       call VecSet(self%initial, const_initial_value, ierr); CHKERRQ(ierr)
+
+    else
+
+       if (mpi%rank /= mpi%input_rank) then
+          allocate(initial_data(count))
+       end if
+       call MPI_bcast(initial_data, count, MPI_DOUBLE_PRECISION, &
+            mpi%input_rank, mpi%comm, ierr)
+       allocate(indices(count))
+       do i = 1, count
+          indices(i) = i-1
+       end do
+       call VecSetValues(self%initial, count, indices, &
+            initial_data, INSERT_VALUES, ierr); CHKERRQ(ierr)
+       call VecAssemblyBegin(self%initial, ierr); CHKERRQ(ierr)
+       call VecAssemblyEnd(self%initial, ierr); CHKERRQ(ierr)
+       deallocate(indices, initial_data)
+
+    end if
+
+    call VecView(self%initial, PETSC_VIEWER_STDOUT_WORLD, ierr); CHKERRQ(ierr)
+
+  end subroutine simulation_read_initial
+
+!------------------------------------------------------------------------
+
+  subroutine simulation_read_timestepping(self, json)
+    !! Reads time stepping data from JSON input.
+
+    class(simulation_type), intent(in out) :: self
+    type(fson_value), pointer, intent(in) :: json
+    ! Locals:
+    type(fson_value), pointer :: time
+    PetscReal, parameter :: default_initial_time = 0.0_dp
+    PetscReal :: initial_time
+
+    if (mpi%rank == mpi%input_rank) then
+        call fson_get(json, "time", time)
+        if (associated(time)) then
+           call fson_get_default(time, "start", default_initial_time, &
+                initial_time)
+        end if
+       
+    end if
+
+    ! call self%timestepper%init(method, self%mesh%dm, simulation_balance, &
+    !      simulation_inflow, initial_time, self%initial, 
+
+  end subroutine simulation_read_timestepping
+
+!------------------------------------------------------------------------
+
   subroutine simulation_init(self, filename)
     !! Initializes a simulation using data from the input file with 
     !! specified name.
@@ -141,6 +271,10 @@ contains
 
     dof = self%eos%num_primary
     call self%mesh%init(json, dof)
+
+    call self%read_initial(json)
+
+    call self%read_timestepping(json)
 
     if (mpi%rank == mpi%input_rank) then
        call fson_destroy(json)
@@ -170,11 +304,46 @@ contains
     ! Locals:
     PetscErrorCode :: ierr
 
+    call VecDestroy(self%initial, ierr); CHKERRQ(ierr)
     call self%mesh%destroy()
     call self%thermo%destroy()
     ! call self%timestepper%destroy()
 
   end subroutine simulation_destroy
+
+!------------------------------------------------------------------------
+
+  subroutine simulation_balance(t, primary, balance)
+    !! Computes mass and energy balance for each cell, for the given
+    !! primary thermodynamic variables and time.
+    PetscReal, intent(in) :: t !! time (s)
+    Vec, intent(in) :: primary
+    Vec, intent(out) :: balance
+    ! Locals:
+    PetscErrorCode :: ierr
+
+    ! Dummy identity function:
+    call VecCopy(primary, balance, ierr); CHKERRQ(ierr)
+
+  end subroutine simulation_balance
+
+!------------------------------------------------------------------------
+
+  subroutine simulation_inflow(t, primary, inflow)
+    !! Computes net inflow into each cell, from flows through faces and
+    !! source terms, for the given primary thermodynamic variables and
+    !! time.
+
+    PetscReal, intent(in) :: t
+    Vec, intent(in) :: primary
+    Vec, intent(out) :: inflow
+    ! Locals:
+    PetscErrorCode :: ierr
+
+    ! Dummy zero inflows:
+    call VecSet(inflow, 0.0_dp, ierr); CHKERRQ(ierr)
+
+  end subroutine simulation_inflow
 
 !------------------------------------------------------------------------
 
