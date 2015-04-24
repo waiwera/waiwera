8e2c53df4bb93d1db4b54823c9b3f1dc494d7724
Adrian Croucher <adrian@eeepc.www.croucher.net>
2015-04-24 13:34:13 +1200

    Added unit test for fluid module

---
+++ test/src/fluid_test.F90
@@ -0,0 +1,73 @@
+module fluid_test
+
+  ! Test for fluid module
+
+  use kinds_module
+  use mpi_module
+  use fruit
+  use fluid_module
+
+  implicit none
+  private
+
+#include <petsc-finclude/petscdef.h>
+
+public :: test_fluid
+
+PetscReal, parameter :: tol = 1.e-6_dp
+
+contains
+  
+!------------------------------------------------------------------------
+
+  subroutine test_fluid
+
+    ! Fluid test
+
+    type(fluid_type) :: fluid
+    PetscInt, parameter :: num_components = 2, num_phases = 2
+    PetscInt,  parameter :: offset = 7
+    PetscReal :: offset_padding(offset) = 0._dp
+    PetscReal, allocatable :: fluid_data(:)
+    PetscInt :: i, p, nc
+
+    if (mpi%rank == mpi%output_rank) then
+
+       call fluid%init(num_components, num_phases)
+
+       allocate(fluid_data(offset + fluid%dof()))
+       do i = 1, size(fluid_data)
+          fluid_data(i) = dble(i)
+       end do
+       call fluid%assign(fluid_data, offset)
+
+       call assert_equals(fluid_data(offset+1), fluid%pressure, tol, "pressure")
+       call assert_equals(fluid_data(offset+2), fluid%temperature, tol, "temperature")
+
+       i = offset + 2
+       do p = 1, num_phases
+          call assert_equals(fluid_data(i+1), &
+               fluid%phase(p)%density, tol, "density")
+          call assert_equals(fluid_data(i+2), &
+               fluid%phase(p)%viscosity, tol, "viscosity")
+          call assert_equals(fluid_data(i+3), &
+               fluid%phase(p)%saturation, tol, "saturation")
+          call assert_equals(fluid_data(i+4), &
+               fluid%phase(p)%relative_permeability, tol, "relative permeability")
+          call assert_equals(fluid_data(i+5), &
+               fluid%phase(p)%specific_enthalpy, tol, "specific enthalpy")
+          nc = size(fluid%phase(p)%mass_fraction)
+          call assert_equals(0._dp, norm2(fluid_data(i+6:i+6+nc) - &
+               fluid%phase(p)%mass_fraction), tol, "mass fraction")
+          i = i + fluid%phase(p)%dof()
+       end do
+
+       deallocate(fluid_data)
+
+    end if
+
+  end subroutine test_fluid
+
+!------------------------------------------------------------------------
+
+end module fluid_test
