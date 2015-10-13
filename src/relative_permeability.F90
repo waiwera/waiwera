19224a622c287ec5526534ddc198c572bc1b6a6c
Adrian Croucher <a.croucher@auckland.ac.nz>
2015-10-13 15:43:02 +1300

    Changed relative_permeability_type init() method to initialize from JSON input, and added routines for reading default relative permeability from input

--- src/relative_permeability.F90
+++ src/relative_permeability.F90
@@ -2,6 +2,7 @@ module relative_permeability_module
   !! Relative permeability functions.
 
   use kinds_module
+  use fson
 
   implicit none
   private
@@ -19,12 +20,20 @@ module relative_permeability_module
           public :: name !! Name of relative permeability curves
    contains
      private
+     procedure(relative_permeability_init_routine), public, deferred :: init
      procedure(relative_permeability_function), public, deferred :: values
   end type relative_permeability_type
 
 !------------------------------------------------------------------------
 
   abstract interface
+
+     subroutine relative_permeability_init_routine(self, json)
+       !! Initializes relative permeability object from JSON data.
+       import :: relative_permeability_type, fson_value
+       class(relative_permeability_type), intent(in out) :: self
+       type(fson_value), pointer, intent(in) :: json
+     end subroutine relative_permeability_init_routine
 
      function relative_permeability_function(self, sl) result(rp)
        !! Relative permeability function, returning array of values for both phases.
@@ -74,22 +83,35 @@ module relative_permeability_module
 
 !------------------------------------------------------------------------
 
+  public :: setup_relative_permeabilities
+
 contains
 
 !------------------------------------------------------------------------
 ! Linear functions
 !------------------------------------------------------------------------
 
-  subroutine relative_permeability_linear_init(self, liquid_limits, &
-       vapour_limits)
+  subroutine relative_permeability_linear_init(self, json)
     !! Initialize linear relative permeability function.
 
+    use fson_mpi_module
+
     class(relative_permeability_linear_type), intent(in out) :: self
-    PetscReal, intent(in) :: liquid_limits(2), vapour_limits(2)
+    type(fson_value), pointer, intent(in) :: json
+    ! Locals:
+    PetscReal, allocatable :: liquid_limits(:), vapour_limits(:)
+    PetscReal, parameter :: default_liquid_limits(2) = [0._dp, 1._dp]
+    PetscReal, parameter :: default_vapour_limits(2) = [0._dp, 1._dp]
 
     self%name = "Linear"
+
+    call fson_get_mpi(json, "liquid", default_liquid_limits, liquid_limits)
+    call fson_get_mpi(json, "vapour", default_vapour_limits, vapour_limits)
+
     self%liquid_limits = liquid_limits
     self%vapour_limits = vapour_limits
+
+    deallocate(liquid_limits, vapour_limits)
 
   end subroutine relative_permeability_linear_init
 
@@ -132,15 +154,20 @@ contains
 ! Corey's curves
 !------------------------------------------------------------------------
 
-  subroutine relative_permeability_corey_init(self, slr, ssr)
+  subroutine relative_permeability_corey_init(self, json)
     !! Initialize Corey's relative permeability function.
 
+    use fson_mpi_module
+
     class(relative_permeability_corey_type), intent(in out) :: self
-    PetscReal, intent(in) :: slr, ssr
+    type(fson_value), pointer, intent(in) :: json
+    ! Locals:
+    PetscReal, parameter :: default_slr = 0.3_dp, default_ssr = 0.6_dp
 
     self%name = "Corey"
-    self%slr = slr
-    self%ssr = ssr
+
+    call fson_get_mpi(json, "slr", default_slr, self%slr)
+    call fson_get_mpi(json, "ssr", default_ssr, self%slr)
 
   end subroutine relative_permeability_corey_init
 
@@ -186,15 +213,20 @@ contains
 ! Grant's curves
 !------------------------------------------------------------------------
 
-  subroutine relative_permeability_grant_init(self, slr, ssr)
+  subroutine relative_permeability_grant_init(self, json)
     !! Initialize Grant's relative permeability function.
 
+    use fson_mpi_module
+
     class(relative_permeability_grant_type), intent(in out) :: self
-    PetscReal, intent(in) :: slr, ssr
+    type(fson_value), pointer, intent(in) :: json
+    ! Locals:
+    PetscReal, parameter :: default_slr = 0.3_dp, default_ssr = 0.6_dp
 
     self%name = "Grant"
-    self%slr = slr
-    self%ssr = ssr
+
+    call fson_get_mpi(json, "slr", default_slr, self%slr)
+    call fson_get_mpi(json, "ssr", default_ssr, self%slr)
 
   end subroutine relative_permeability_grant_init
 
@@ -222,6 +254,68 @@ contains
     end if
 
   end function relative_permeability_grant_values
+
+!------------------------------------------------------------------------
+
+  subroutine setup_relative_permeability(json, rp)
+    !! Sets up single relative permeability object from JSON object
+    !! for rock data in input.
+
+    use fson_mpi_module
+    use utils_module, only: str_to_lower
+
+    type(fson_value), pointer, intent(in) :: json
+    class(relative_permeability_type), allocatable, intent(out) :: rp
+    ! Locals:
+    character(max_relative_permeability_name_length), parameter :: &
+         default_relperm_type = "Linear"
+    character(max_relative_permeability_name_length) :: relperm_type
+
+    call fson_get_mpi(json, "type", default_relperm_type, relperm_type)
+
+    select case (str_to_lower(relperm_type))
+    case ("linear")
+       allocate(relative_permeability_linear_type :: rp)
+    case ("corey")
+       allocate(relative_permeability_corey_type :: rp)
+    case ("grant")
+       allocate(relative_permeability_grant_type :: rp)
+    case default
+       allocate(relative_permeability_linear_type :: rp)
+    end select
+    call rp%init(json)
+
+  end subroutine setup_relative_permeability
+
+!------------------------------------------------------------------------
+
+  subroutine setup_relative_permeabilities(json, rp)
+    !! Sets up relative permeability objects from rock data in JSON input.
+    !! Eventually rp will be an array of objects.
+
+    use fson_mpi_module
+
+    type(fson_value), pointer, intent(in) :: json
+    class(relative_permeability_type), allocatable, intent(out) :: rp
+    ! Locals:
+    type(fson_value), pointer :: relperm
+    PetscBool :: default_present
+
+    if (fson_has_mpi(json, "rock.relative permeability")) then
+       call fson_get_mpi(json, "rock.relative permeability", relperm)
+       default_present = .true.
+    else
+       relperm => fson_parse(str = "{}")
+       default_present = .false.
+    end if
+
+    call setup_relative_permeability(relperm, rp)
+
+    if (.not. (default_present)) then
+       call fson_destroy(relperm)
+    end if
+
+  end subroutine setup_relative_permeabilities
 
 !------------------------------------------------------------------------
 
