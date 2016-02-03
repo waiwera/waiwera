50203e9da7f25252848a6bca7e16b049032adb50
Adrian Croucher <a.croucher@auckland.ac.nz>
2016-02-03 17:39:36 +1300

    Exclude ghost cells from point location in mesh order_vector()

--- src/mesh.F90
+++ src/mesh.F90
@@ -454,57 +454,66 @@ contains
 
     use cell_module, only: cell_type
     use dm_utils_module, only: local_vec_section, section_offset, &
-         set_dm_data_layout
+         set_dm_data_layout, global_vec_section
 
     class(mesh_type), intent(in) :: self
     Vec, intent(in) :: geom
     Vec, intent(in out) :: v
     ! Locals:
-    DM :: geom_dm, centroid_dm
     Vec :: vinitial, centroids
     type(cell_type) :: cell
-    IS :: cell_indices
+    IS :: cell_indices, cell_target_indices
     VecScatter :: scatter
     PetscReal, pointer :: geom_array(:), centroids_array(:)
-    PetscSection :: geom_section, centroid_section
-    PetscInt :: geom_offset, dim, c, centroid_offset
+    PetscSection :: geom_section
+    PetscInt :: geom_offset, dim, c, num_cells, n, ghost
+    DMLabel :: ghost_label
     PetscErrorCode :: ierr
 
     call VecDuplicate(v, vinitial, ierr); CHKERRQ(ierr)
     call VecCopy(v, vinitial, ierr); CHKERRQ(ierr)
 
-    ! Extract centroids from geometry:
     call local_vec_section(geom, geom_section)
     call VecGetArrayReadF90(geom, geom_array, ierr); CHKERRQ(ierr)
+
     call DMGetDimension(self%dm, dim, ierr); CHKERRQ(ierr)
-    call VecGetDM(geom, geom_dm, ierr); CHKERRQ(ierr)
-    call DMClone(geom_dm, centroid_dm, ierr); CHKERRQ(ierr)
-    call set_dm_data_layout(centroid_dm, [dim], [dim])
-    call DMCreateLocalVector(centroid_dm, centroids, ierr)
+    call DMGetLabel(self%dm, "ghost", ghost_label, ierr)
+    num_cells = 0
+    do c = self%start_cell, self%end_interior_cell - 1
+       call DMLabelGetValue(ghost_label, c, ghost, ierr); CHKERRQ(ierr)
+       if (ghost < 0) then
+          num_cells = num_cells + 1
+       end if
+    end do
+    call VecCreateSeq(PETSC_COMM_SELF, num_cells * dim, centroids, ierr)
     CHKERRQ(ierr)
-    call local_vec_section(centroids, centroid_section)
+    call VecSetBlockSize(centroids, dim, ierr); CHKERRQ(ierr)
+
     call VecGetArrayF90(centroids, centroids_array, ierr); CHKERRQ(ierr)
-    do c = self%start_cell, self%end_cell - 1
-       call section_offset(geom_section, c, geom_offset, ierr)
-       CHKERRQ(ierr)
-       call cell%assign(geom_array, geom_offset)
-       call section_offset(centroid_section, c, centroid_offset, ierr)
-       CHKERRQ(ierr)
-       centroids_array(centroid_offset: &
-            centroid_offset + dim - 1) = cell%centroid
+    CHKERRQ(ierr)
+    n = 1
+    do c = self%start_cell, self%end_interior_cell - 1
+       call DMLabelGetValue(ghost_label, c, ghost, ierr); CHKERRQ(ierr)
+       if (ghost < 0) then
+          call section_offset(geom_section, c, geom_offset, ierr)
+          CHKERRQ(ierr)
+          call cell%assign(geom_array, geom_offset)
+          centroids_array(n: n + dim - 1) = cell%centroid
+          n = n + dim
+       end if
     end do
     call VecRestoreArrayF90(centroids,centroids_array, ierr)
     CHKERRQ(ierr)
-    call VecView(centroids, PETSC_VIEWER_STDOUT_WORLD, ierr)
     call VecRestoreArrayReadF90(geom, geom_array, ierr); CHKERRQ(ierr)
-    call PetscSectionDestroy(centroid_section, ierr); CHKERRQ(ierr)
+    call ISCreateStride(mpi%comm, num_cells, 0, 1, cell_target_indices, &
+         ierr); CHKERRQ(ierr)
 
     ! Locate centroids in DM, and scatter:
     call DMLocatePoints(self%dm, centroids, cell_indices, ierr)
     CHKERRQ(ierr)
-    call ISView(cell_indices, PETSC_VIEWER_STDOUT_WORLD, ierr)  ! debug
+
     call VecScatterCreate(vinitial, cell_indices, &
-         v, PETSC_NULL_OBJECT, scatter, ierr); CHKERRQ(ierr)
+         v, cell_target_indices, scatter, ierr); CHKERRQ(ierr)
     call VecScatterBegin(scatter, vinitial, v, &
          INSERT_VALUES, SCATTER_FORWARD, ierr); CHKERRQ(ierr)
     call VecScatterEnd(scatter, vinitial, v, &
@@ -512,8 +521,8 @@ contains
 
     call VecScatterDestroy(scatter, ierr); CHKERRQ(ierr)
     call ISDestroy(cell_indices, ierr); CHKERRQ(ierr)
+    call ISDestroy(cell_target_indices, ierr); CHKERRQ(ierr)
     call VecDestroy(centroids, ierr); CHKERRQ(ierr)
-    call DMDestroy(centroid_dm, ierr); CHKERRQ(ierr)
     call VecDestroy(vinitial, ierr); CHKERRQ(ierr)
 
   end subroutine mesh_order_vector
