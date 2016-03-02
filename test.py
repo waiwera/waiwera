7abe4928a4351857a97582f378b19aea24527854
Adrian Croucher <a.croucher@auckland.ac.nz>
2016-03-02 13:47:28 +1300

    Changed unit test script so that unit tests are all done on 1, 2, 3 and 4 processors (instead of just 4)

--- test.py
+++ test.py
@@ -12,7 +12,7 @@ output_dir = 'test/dist/'
 testsuffix = '_test'
 f90 = '.F90'
 setup_source = test_dir + 'setup' + testsuffix + f90
-num_procs = 4
+num_procs = [1, 2, 3, 4]
 mpi_comm = 'PETSC_COMM_WORLD'
 
 if len(argv) > 1: test_sources = [test_dir + arg + testsuffix + f90 for arg in argv[1:]]
@@ -23,8 +23,11 @@ if driver_source in test_sources: test_sources.remove(driver_source)
 if setup_source not in test_sources: test_sources.append(setup_source)
 
 suite = FRUIT.test_suite(test_sources)
-suite.build_run(driver_source, build_command, output_dir = output_dir,
-                num_procs = num_procs, mpi_comm = mpi_comm)
 
-if suite.built: suite.summary()
-else: print 'Failed to build/run tests'
+for np in num_procs:
+    print '-'*72
+    print 'Processors:', np
+    suite.build_run(driver_source, build_command, output_dir = output_dir,
+                    num_procs = np, mpi_comm = mpi_comm, mpi = True)
+    if suite.built: suite.summary()
+    else: print 'Failed to build/run tests'
