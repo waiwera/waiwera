33b187e22705d4c8f7644c740deff08d9d89f652
Adrian Croucher <a.croucher@auckland.ac.nz>
2016-09-26 18:22:15 +1300

    Divided setup_sources() routine into smaller routines

--- src/source_setup.F90
+++ src/source_setup.F90
@@ -1,7 +1,15 @@
 module source_setup_module
   !! Module for setting up sources and sinks.
 
+  use kinds_module
+  use fson
+  use fson_value_m, only: TYPE_REAL, TYPE_ARRAY
+  use fson_mpi_module
+  use list_module
+  use logfile_module
+  use mesh_module, only: cell_order_label_name
   use source_module
+  use source_control_module
 
   implicit none
   private
@@ -18,13 +26,6 @@ contains
        source_controls_list, logfile)
     !! Sets up lists of sinks / sources and source controls.
 
-    use kinds_module
-    use fson
-    use fson_mpi_module
-    use list_module
-    use logfile_module
-    use mesh_module, only: cell_order_label_name
-
     type(fson_value), pointer, intent(in) :: json !! JSON file object
     DM, intent(in) :: dm !! Mesh DM
     PetscInt, intent(in) :: np !! Number of primary variables
@@ -34,10 +35,11 @@ contains
     type(logfile_type), intent(in out), optional :: logfile !! Logfile for log output
     ! Locals:
     PetscErrorCode :: ierr
-    PetscInt :: c, isrc, i, injection_component, production_component
-    type(fson_value), pointer :: sources, src
+    PetscInt :: c, isrc, injection_component, production_component
+    type(fson_value), pointer :: sources_json, source_json
     PetscInt :: num_sources, cell, num_cells, ghost
-    type(source_type), pointer :: s
+    type(source_type), pointer :: source
+    class(source_control_type), pointer :: control
     PetscInt, pointer :: cells(:)
     PetscReal :: rate, enthalpy
     character(max_source_name_length) :: name
@@ -55,44 +57,27 @@ contains
 
        call DMGetLabel(dm, "ghost", ghost_label, ierr); CHKERRQ(ierr)
 
-       call fson_get_mpi(json, "source", sources)
-       num_sources = fson_value_count_mpi(sources, ".")
+       call fson_get_mpi(json, "source", sources_json)
+       num_sources = fson_value_count_mpi(sources_json, ".")
 
        do isrc = 1, num_sources
 
           write(istr, '(i0)') isrc - 1
           srcstr = 'source[' // trim(istr) // '].'
-          src => fson_value_get_mpi(sources, isrc)
-          call fson_get_mpi(src, "name", default_name, name)
-          call fson_get_mpi(src, "cell", val = cell)
+          source => null()
 
-          if (fson_has_mpi(src, "rate")) then
-             call fson_get_mpi(src, "rate", val = rate)
-             can_inject = (rate > 0._dp)
-          else
-             rate = default_source_rate
-             can_inject = PETSC_TRUE
-          end if
+          source_json => fson_value_get_mpi(sources_json, isrc)
 
-          call fson_get_mpi(src, "component", default_source_component, &
-               injection_component, logfile, trim(srcstr) // "component")
-          if (fson_has_mpi(src, "production_component")) then
-             call fson_get_mpi(src, "production_component", &
-                  val = production_component)
-          else
-             if ((.not. isothermal) .and. (injection_component == np)) then
-                production_component = injection_component
-             else
-                production_component = default_source_production_component
-             end if
-          end if
+          call fson_get_mpi(source_json, "name", default_name, name)
+          call fson_get_mpi(source_json, "cell", val = cell)
 
-          if (can_inject .and. (injection_component < np)) then
-             call fson_get_mpi(src, "enthalpy", default_source_injection_enthalpy, &
-                  enthalpy, logfile, trim(srcstr) // "enthalpy")
-          else
-             enthalpy = 0._dp
-          end if
+          call get_rate(source_json, rate, can_inject)
+
+          call get_components(source_json, isothermal, np, srcstr, &
+               injection_component, production_component, logfile)
+
+          call get_enthalpy(source_json, isothermal, can_inject, &
+               injection_component, np, srcstr, enthalpy, logfile)
 
           call DMGetStratumIS(dm, cell_order_label_name, &
                cell, cell_IS, ierr); CHKERRQ(ierr)
@@ -100,28 +85,29 @@ contains
 
              call ISGetIndicesF90(cell_IS, cells, ierr); CHKERRQ(ierr)
              num_cells = size(cells)
-             if (num_cells > 1) then
-                num_cells = 1
-                if (present(logfile)) then
-                   call logfile%write(LOG_LEVEL_WARN, 'source', &
-                        'multiple cells found for source: ' // trim(name))
-                end if
+             if ((num_cells > 1) .and. present(logfile)) then
+                call logfile%write(LOG_LEVEL_WARN, 'source', &
+                     'multiple cells found for source: ' // trim(name))
              end if
-             do i = 1, num_cells
-                c = cells(i)
-                call DMLabelGetValue(ghost_label, c, ghost, ierr)
-                CHKERRQ(ierr)
-                if (ghost < 0) then
-                   allocate(s)
-                   call s%init(cell, c, np, rate, enthalpy, &
-                        injection_component, production_component)
-                   call sources_list%append(s, name)
-                end if
-             end do
+
+             c = cells(1)
+             call DMLabelGetValue(ghost_label, c, ghost, ierr)
+             CHKERRQ(ierr)
+             if (ghost < 0) then
+                allocate(source)
+                call source%init(cell, c, np, rate, enthalpy, &
+                     injection_component, production_component)
+                call sources_list%append(source, name)
+             end if
              call ISRestoreIndicesF90(cell_IS, cells, ierr); CHKERRQ(ierr)
 
           end if
           call ISDestroy(cell_IS, ierr); CHKERRQ(ierr)
+
+          if (associated(source)) then
+             ! Set up any inline controls for this source:
+
+          end if
 
        end do
 
@@ -132,6 +118,99 @@ contains
     end if
 
   end subroutine setup_sources
+
+!------------------------------------------------------------------------
+
+  subroutine get_rate(source_json, rate, can_inject)
+    !! Gets flow rate and whether the source can inject. These can
+    !! only be determined for constant-rate sources. For other source
+    !! types, a default initial rate is assigned and it is assumed
+    !! that the source may inject.
+
+    type(fson_value), pointer, intent(in) :: source_json
+    PetscReal, intent(out) :: rate
+    PetscBool, intent(out) :: can_inject
+    ! Locals:
+    PetscInt :: rate_type
+
+    if (fson_has_mpi(source_json, "rate")) then
+
+       rate_type = fson_type_mpi(source_json, "rate")
+
+       if (rate_type == TYPE_REAL) then
+          call fson_get_mpi(source_json, "rate", val = rate)
+          can_inject = (rate > 0._dp)
+       else if (rate_type == TYPE_ARRAY) then
+          rate = default_source_rate
+          can_inject = PETSC_TRUE
+       end if
+
+    else
+       rate = default_source_rate
+       can_inject = PETSC_TRUE
+    end if
+
+  end subroutine get_rate
+
+!------------------------------------------------------------------------
+
+  subroutine get_components(source_json, isothermal, &
+       np, srcstr, injection_component, production_component, &
+       logfile)
+    !! Gets injection and production components for the source.
+
+    type(fson_value), pointer, intent(in) :: source_json
+    PetscBool, intent(in) :: isothermal
+    PetscInt, intent(in) :: np
+    character(len=*) :: srcstr
+    PetscInt, intent(out) :: injection_component
+    PetscInt, intent(out) :: production_component
+    type(logfile_type), intent(in out), optional :: logfile
+
+    call fson_get_mpi(source_json, "component", default_source_component, &
+         injection_component, logfile, trim(srcstr) // "component")
+
+    if (fson_has_mpi(source_json, "production_component")) then
+
+       call fson_get_mpi(source_json, "production_component", &
+            val = production_component)
+
+    else
+
+       if ((.not. isothermal) .and. (injection_component == np)) then
+          production_component = injection_component
+       else
+          production_component = default_source_production_component
+       end if
+
+    end if
+
+  end subroutine get_components
+
+!------------------------------------------------------------------------
+
+  subroutine get_enthalpy(source_json, isothermal, can_inject, &
+       injection_component, np, srcstr, enthalpy, logfile)
+    !! Gets production enthalpy for the source, if needed.
+
+    type(fson_value), pointer, intent(in) :: source_json
+    PetscBool, intent(in) :: isothermal, can_inject
+    PetscInt, intent(in) :: injection_component, np
+    character(len=*) :: srcstr
+    PetscReal, intent(out) :: enthalpy
+    type(logfile_type), intent(in out), optional :: logfile
+
+    if (can_inject .and. (injection_component < np)) then
+
+       call fson_get_mpi(source_json, "enthalpy", &
+            default_source_injection_enthalpy, &
+            enthalpy, logfile, trim(srcstr) // "enthalpy")
+
+    else
+       enthalpy = 0._dp
+    end if
+
+  end subroutine get_enthalpy
 
 !------------------------------------------------------------------------
 
