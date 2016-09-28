3aa832d2ca2ae07d9fba84c9a3eeb57ee99885f7
Adrian Croucher <a.croucher@auckland.ac.nz>
2016-09-28 15:59:03 +1300

    Use eos object in test_setup_sources()

--- test/src/source_setup_test.F90
+++ test/src/source_setup_test.F90
@@ -5,6 +5,7 @@ module source_setup_test
   use kinds_module
   use mpi_module
   use fruit
+  use eos_module
   use source_setup_module
 
   implicit none
@@ -12,9 +13,140 @@ module source_setup_test
 
 #include <petsc/finclude/petsc.h90>
 
+  type, extends(eos_type) :: eos_test_type
+     !! EOS used only for testing source setup.
+   contains
+     private
+     procedure, public :: init => eos_test_init
+     procedure, public :: transition => eos_test_transition
+     procedure, public :: bulk_properties => eos_test_bulk_properties
+     procedure, public :: phase_properties => eos_test_phase_properties
+     procedure, public :: primary_variables => eos_test_primary_variables
+     procedure, public :: check_primary_variables => eos_test_check_primary_variables
+  end type eos_test_type
+
 public :: test_setup_sources
 
 contains
+
+!------------------------------------------------------------------------
+
+  subroutine eos_test_init(self, json, thermo, logfile)
+
+    use fson
+    use fson_mpi_module, only: fson_get_mpi
+    use logfile_module
+    use thermodynamics_module
+
+    class(eos_test_type), intent(in out) :: self
+    type(fson_value), pointer, intent(in) :: json
+    class(thermodynamics_type), intent(in), target :: thermo
+    type(logfile_type), intent(in out), optional :: logfile
+    ! Locals:
+    PetscReal, parameter :: default_pressure = 1.0e5_dp
+    PetscReal, parameter :: default_temperature = 20._dp
+    PetscReal, parameter :: default_gas_partial_pressure = 0._dp
+
+    self%name = "test"
+    self%description = "Test EOS"
+    self%primary_variable_names = [&
+         "pressure                     ", &
+         "temperature/vapour_saturation", &
+         "gas partial pressure         "]
+
+    self%num_primary_variables = size(self%primary_variable_names)
+    self%num_phases = 2
+    self%phase_names = ["liquid", "vapour"]
+    self%num_components = 2
+    self%component_names = ["water", "gas  "]
+
+    self%default_primary = [default_pressure, default_temperature, &
+         default_gas_partial_pressure]
+    self%default_region = 1
+
+    self%thermo => thermo
+
+  end subroutine eos_test_init
+
+!------------------------------------------------------------------------
+
+  subroutine eos_test_transition(self, primary, old_fluid, fluid, &
+       transition, err)
+
+    use fluid_module, only: fluid_type
+
+    class(eos_test_type), intent(in out) :: self
+    PetscReal, intent(in out) :: primary(self%num_primary_variables)
+    type(fluid_type), intent(in) :: old_fluid
+    type(fluid_type), intent(in out) :: fluid
+    PetscBool, intent(out) :: transition
+    PetscErrorCode, intent(out) :: err
+
+    transition = PETSC_FALSE
+    err = 0
+
+  end subroutine eos_test_transition
+
+!------------------------------------------------------------------------
+
+  subroutine eos_test_bulk_properties(self, primary, fluid, err)
+
+    use fluid_module, only: fluid_type
+
+    class(eos_test_type), intent(in out) :: self
+    PetscReal, intent(in) :: primary(self%num_primary_variables)
+    type(fluid_type), intent(in out) :: fluid
+    PetscErrorCode, intent(out) :: err
+
+    err = 0
+
+  end subroutine eos_test_bulk_properties
+
+!------------------------------------------------------------------------
+
+  subroutine eos_test_phase_properties(self, primary, rock, fluid, err)
+
+    use rock_module, only: rock_type
+    use fluid_module, only: fluid_type
+
+    class(eos_test_type), intent(in out) :: self
+    PetscReal, intent(in) :: primary(self%num_primary_variables)
+    type(rock_type), intent(in out) :: rock
+    type(fluid_type), intent(in out) :: fluid
+    PetscErrorCode, intent(out) :: err
+
+    err = 0
+
+  end subroutine eos_test_phase_properties
+
+!------------------------------------------------------------------------
+
+  subroutine eos_test_primary_variables(self, fluid, primary)
+
+    use fluid_module, only: fluid_type
+
+    class(eos_test_type), intent(in) :: self
+    type(fluid_type), intent(in) :: fluid
+    PetscReal, intent(out) :: primary(self%num_primary_variables)
+
+    primary = 0._dp
+
+  end subroutine eos_test_primary_variables
+
+!------------------------------------------------------------------------
+
+  PetscErrorCode function eos_test_check_primary_variables(self, fluid, &
+       primary) result(err)
+
+    use fluid_module, only: fluid_type
+
+    class(eos_test_type), intent(in) :: self
+    type(fluid_type), intent(in) :: fluid
+    PetscReal, intent(in) :: primary(self%num_primary_variables)
+
+    err = 0
+
+  end function eos_test_check_primary_variables
 
 !------------------------------------------------------------------------
 
@@ -27,37 +159,32 @@ contains
     use mesh_module
     use list_module
     use source_module
-    use eos_module, only: max_primary_variable_name_length
+    use IAPWS_module
 
     character(16), parameter :: path = "data/source/"
-    PetscInt, parameter :: nc = 2
-    PetscInt :: np
-    character(max_primary_variable_name_length), allocatable :: &
-         primary_variable_names(:)
-    PetscBool :: isothermal
+    type(IAPWS_type) :: thermo
+    type(eos_test_type) :: eos
     type(fson_value), pointer :: json
     type(mesh_type) :: mesh
     type(list_type) :: sources, source_controls
     PetscErrorCode :: ierr
 
-    primary_variable_names = &
-         [character(max_primary_variable_name_length) :: &
-         "P1", "P2", "T"]
-    np = size(primary_variable_names)
-
-    isothermal = (nc == np)
-
     json => fson_parse_mpi(trim(path) // "test_source.json")
 
+    call thermo%init()
+    call eos%init(json, thermo)
     call mesh%init(json)
     call DMCreateLabel(mesh%dm, open_boundary_label_name, ierr); CHKERRQ(ierr)
-    call mesh%configure(primary_variable_names)
+    call mesh%configure(eos%primary_variable_names)
 
-    call setup_sources(json, mesh%dm, np, isothermal, sources, source_controls)
+    call setup_sources(json, mesh%dm, eos, sources, source_controls)
+
     call sources%traverse(source_test_iterator)
-
     call sources%destroy(source_list_node_data_destroy)
+
     call mesh%destroy()
+    call eos%destroy()
+    call thermo%destroy()
     call fson_destroy_mpi(json)
 
   contains
