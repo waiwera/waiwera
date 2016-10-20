143a5f9b06002e232fb7331707a652f5484dd757
Adrian Croucher <a.croucher@auckland.ac.nz>
2016-10-20 14:54:40 +1300

    Added recharge source control benchmark test

---
+++ test/benchmark/source/recharge/test_recharge.py
@@ -0,0 +1,179 @@
+"""
+Tests for recharge sources
+"""
+
+import os
+
+from credo.systest import SciBenchmarkTest
+from credo.systest import FieldWithinTolTC
+from credo.systest import HistoryWithinTolTC
+
+from credo.jobrunner import SimpleJobRunner
+from credo.t2model import T2ModelRun, T2ModelResult
+from credo.supermodel import SuperModelRun
+
+import credo.reporting.standardReports as sReps
+from credo.reporting import getGenerators
+
+from mulgrids import mulgrid
+import matplotlib.pyplot as plt
+import numpy as np
+
+from docutils.core import publish_file
+
+model_name = 'recharge'
+
+AUT2_FIELDMAP = {
+    'Pressure': 'Pressure',
+    'Temperature': 'Temperature',
+    'Vapour saturation': 'Vapour saturation',
+}
+SUPER_FIELDMAP = {
+    'Pressure': 'fluid_pressure',
+    'Temperature': 'fluid_temperature',
+    'Vapour saturation': 'fluid_vapour_saturation',
+}
+
+model_dir = './run'
+t2geo_filename = os.path.join(model_dir, 'g' + model_name + '.dat')
+
+num_procs = 1
+
+run_names = ['case_1']
+test_fields = ["Pressure", "Temperature"]
+plot_fields = ["Pressure"]
+
+geo = mulgrid(t2geo_filename)
+map_out_atm = range(geo.num_atmosphere_blocks, geo.num_blocks)
+
+test = SciBenchmarkTest("Recharge", nproc = num_procs)
+test.description = """Tests recharge sources"""
+
+for run_index, run_name in enumerate(run_names):
+
+    run_base_name = '_'.join((model_name, run_name))
+    run_filename = run_base_name + '.json'
+    model_run = SuperModelRun(run_name, run_filename,
+                              fieldname_map = SUPER_FIELDMAP,
+                              simulator = 'supermodel',
+                              basePath = os.path.realpath(model_dir))
+    model_run.jobParams['nproc'] = num_procs
+    test.mSuite.addRun(model_run, run_name)
+
+test.setupEmptyTestCompsList()
+
+for run_index, run_name in enumerate(run_names):
+
+    run_base_name = '_'.join((model_name, run_name))
+    run_filename = os.path.join(model_dir, run_base_name + ".listing")
+    reference_result = T2ModelResult("aut2", run_filename,
+                                     geo_filename = t2geo_filename,
+                                     ordering_map = map_out_atm,
+                                     fieldname_map = AUT2_FIELDMAP)
+    test.addTestComp(run_index, "final errors",
+                                        FieldWithinTolTC(fieldsToTest = test_fields,
+                                                         defFieldTol = 1.e-4,
+                                                         expected = reference_result,
+                                                         testOutputIndex = -1))
+    test.addTestComp(run_index, "time history LH end",
+                                    HistoryWithinTolTC(fieldsToTest = test_fields,
+                                                       defFieldTol = 1.e-3,
+                                                       expected = reference_result,
+                                                       testCellIndex = 0))
+    test.addTestComp(run_index, "time history RH end",
+                                    HistoryWithinTolTC(fieldsToTest = test_fields,
+                                                       defFieldTol = 1.e-3,
+                                                       expected = reference_result,
+                                                       testCellIndex = -1))
+    
+jrunner = SimpleJobRunner(mpi = True)
+testResult, mResults = test.runTest(jrunner, createReports = True)
+
+# plots:
+x = [col.centre[0] for col in geo.columnlist]
+
+for run_index, run_name in enumerate(run_names):
+
+    run_base_name = '_'.join((model_name, run_name))
+
+    tc_name = "time history LH end"
+    t = test.testComps[run_index][tc_name].times
+    for field_name in plot_fields:
+        var = np.array(test.testComps[run_index][tc_name].fieldErrors[field_name])
+        plt.plot(t, var, '-o')
+        plt.xlabel('t (s)')
+        plt.ylabel(field_name + ' error')
+        plt.title(run_name + ' ' + tc_name)
+        img_filename_base = '_'.join((run_base_name, tc_name, 'error', field_name))
+        img_filename_base = img_filename_base.replace(' ', '_')
+        img_filename = os.path.join(test.mSuite.runs[run_index].basePath,
+                                    test.mSuite.outputPathBase,
+                                    img_filename_base + '.png')
+        plt.savefig(img_filename)
+        plt.clf()
+        test.mSuite.analysisImages.append(img_filename)
+
+    tc_name = "time history RH end"
+    t = test.testComps[run_index][tc_name].times
+    for field_name in plot_fields:
+        var = np.array(test.testComps[run_index][tc_name].fieldErrors[field_name])
+        plt.plot(t, var, '-o')
+        plt.xlabel('t (s)')
+        plt.ylabel(field_name + ' error')
+        plt.title(run_name + ' ' + tc_name)
+        img_filename_base = '_'.join((run_base_name, tc_name, 'error', field_name))
+        img_filename_base = img_filename_base.replace(' ', '_')
+        img_filename = os.path.join(test.mSuite.runs[run_index].basePath,
+                                    test.mSuite.outputPathBase,
+                                    img_filename_base + '.png')
+        plt.savefig(img_filename)
+        plt.clf()
+        test.mSuite.analysisImages.append(img_filename)
+
+    # plot time history results at RH end cell:
+    for field_name in plot_fields:
+        var = test.mSuite.resultsList[run_index].getFieldHistoryAtCell(field_name, -1)
+        plt.semilogx(t, var, '-o', label = 'supermodel')
+        var = reference_result.getFieldHistoryAtCell(field_name, -1)
+        plt.semilogx(t, var, 's', label = 'AUTOUGH2')
+        plt.xlabel('t (s)')
+        plt.ylabel(field_name)
+        plt.legend()
+        plt.title(run_name + ' ' + tc_name + ' results')
+        img_filename_base = '_'.join((run_base_name, tc_name, field_name))
+        img_filename_base = img_filename_base.replace(' ', '_')
+        img_filename = os.path.join(test.mSuite.runs[run_index].basePath,
+                                    test.mSuite.outputPathBase,
+                                    img_filename_base + '.png')
+        plt.savefig(img_filename)
+        plt.clf()
+        test.mSuite.analysisImages.append(img_filename)
+
+    tc_name = "final errors"
+    for field_name in plot_fields:
+        var = np.array(test.testComps[run_index][tc_name].fieldErrors[field_name])
+        plt.plot(x, var, 'o-')
+        plt.xlabel('x (m)')
+        plt.ylabel(field_name + ' error')
+        plt.title(run_name + ' ' + tc_name)
+        img_filename_base = '_'.join((run_base_name, tc_name, field_name))
+        img_filename_base = img_filename_base.replace(' ', '_')
+        img_filename = os.path.join(test.mSuite.runs[run_index].basePath,
+                                    test.mSuite.outputPathBase,
+                                    img_filename_base + '.png')
+        plt.savefig(img_filename)
+        plt.clf()
+        test.mSuite.analysisImages.append(img_filename)
+
+# generate report:
+
+for rGen in getGenerators(["RST"], test.outputPathBase):
+    report_filename = os.path.join(test.outputPathBase,
+                     "%s-report.%s" % (test.testName, rGen.stdExt))
+    sReps.makeSciBenchReport(test, mResults, rGen, report_filename)
+    html_filename = os.path.join(test.outputPathBase,
+                     "%s-report.%s" % (test.testName, 'html'))
+    html = publish_file(source_path = report_filename,
+                        destination_path = html_filename,
+                        writer_name = "html")
+
