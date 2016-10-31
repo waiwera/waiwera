4972ee1b2a1cfa8b9e2e68031b3ea156b16a0ceb
Adrian Croucher <a.croucher@auckland.ac.nz>
2016-10-31 14:49:41 +1300

    Added benchmark test for Model Intercomparison Study problem 5

---
+++ test/benchmark/model_intercomparison_study/problem5/setup.py
@@ -0,0 +1,131 @@
+from t2data_json import *
+from t2incons import *
+from t2thermo import cowat
+import json
+import os
+
+model_dir = './run'
+orig_dir = os.getcwd()
+if not os.path.isdir(model_dir): os.makedirs(model_dir)
+os.chdir(model_dir)
+
+model_name = 'problem5'
+dimensions = [300., 200., 100.]
+nblks = [12, 8, 1]
+gridsizes = [dim / nblk for dim, nblk in zip(dimensions, nblks)]
+dx = [[gridsize] * nblk for gridsize, nblk in zip(gridsizes, nblks)]
+
+geo = mulgrid().rectangular(dx[0], dx[1], dx[2], atmos_type = 2)
+geo.write('g'+ model_name +'.dat')
+
+dat = t2data_export_json()
+dat.title = 'Model intercomparison study problem 5'
+dat.simulator = 'AUTOUGH2.2'
+
+dat.grid = t2grid().fromgeo(geo)
+rock = dat.grid.rocktypelist[0]
+rock.porosity = 0.35
+rock.density = 2500.
+rock.permeability = np.ones(3) * 25.e-15
+rock.conductivity = 1.0
+rock.specific_heat = 1000.
+
+dat.relative_permeability = {'type': 3, 'parameters': [0.3, 0.1, 0., 0., 0.]}
+dat.capillarity = {'type': 1, 'parameters': [0., 0., 1., 0., 0.]}
+
+ndt = 200
+yr = 365. * 24. * 60 * 60.
+P0, T0 = 36.e5, 240.
+dat.parameter.update(
+    {'max_timesteps': ndt,
+     'tstop': 10. * yr,
+     'print_interval': 1,
+     'gravity': 9.81,
+     'default_incons': [P0, T0],
+     'const_timestep': 0.05 * yr,
+     'max_timestep': 0.05 * yr,
+     'relative_error': 1.e-6,
+     'absolute_error': 1.
+     })
+
+dat.parameter['option'][1] = 1
+dat.parameter['option'][11] = 2 # permeability weighting
+dat.parameter['option'][12] = 1 # generation table interpolation
+dat.parameter['option'][16] = 5
+dat.parameter['option'][24] = 2 # initial output
+
+dat.multi = {'eos': 'EW', 'num_components': 1, 'num_phases': 2,
+             'num_equations': 2, 'num_secondary_parameters': 6}
+
+lay = geo.layerlist[-1]
+
+genpos = np.array([62.5, 62.5])
+col = geo.column_containing_point(genpos)
+genrate = -50.e-3 * dimensions[2]
+blkname = geo.block_name(lay.name, col.name)
+gen = t2generator(name = blkname, block = blkname,
+                  gx = genrate, type = 'MASS')
+dat.add_generator(gen)
+
+# BC blocks at x = 300:
+rect = [np.array([dimensions[0] - gridsizes[0], 0.]), np.array([dimensions[0], dimensions[1]])]
+cols = geo.columns_in_polygon(rect)
+for i, col in enumerate(cols):
+    blkname = geo.block_name(lay.name, col.name)
+    blk = dat.grid.block[blkname]
+    bcblk = t2block('bc %2d' % i, geo.atmosphere_volume, rock,
+                    np.array([dimensions[0], col.centre[1], -0.5 * dimensions[2]]))
+    dat.grid.add_block(bcblk)
+    con = t2connection([blk, bcblk], 1, [0.5 * gridsizes[0], geo.atmosphere_connection],
+                       gridsizes[1] * gridsizes[2], 0.)
+    dat.grid.add_connection(con)
+
+dat.write(model_name + 'a.dat')
+
+# incons:
+def initial_temp(pos):
+    r = np.linalg.norm(pos)
+    if r <= 100.: T = 240.
+    elif r < 300.:
+        R = (r - 100.) / 200.
+        T = 240. - 160. * R * R + 80. * R**4
+    else: T = 160.
+    return T
+inc = t2incon()
+for blk in dat.grid.blocklist:
+    T0 = initial_temp(blk.centre[:2])
+    inc[blk.name] = [P0, T0]
+inc.write(model_name + '.incon')
+
+dat.run(simulator = 'AUTOUGH2_41Da',
+        incon_filename = model_name + '.incon',
+        silent = True)
+
+mesh_filename = 'g' + model_name + '.exo'
+geo.write_exodusii(mesh_filename)
+jsondata = dat.json(geo, mesh_filename, incons = inc, bdy_incons = inc)
+json.dump(jsondata, file(model_name + 'a.json', 'w'), indent = 2)
+
+# problem 5b:
+
+genpos = np.array([162.5, 137.5])
+col = geo.column_containing_point(genpos)
+genrate = 30.e-3 * dimensions[2]
+d,u = cowat(80., P0)
+h = u + P0 / d
+blkname = geo.block_name(lay.name, col.name)
+gen = t2generator(name = blkname, block = blkname, type = 'MASS',
+                  ex = h, ltab = 3,
+                  time = [0., yr, dat.parameter['tstop']],
+                  rate = [0., genrate, genrate])
+dat.add_generator(gen)
+
+dat.write(model_name + 'b.dat')
+dat.run(simulator = 'AUTOUGH2_41Da',
+        incon_filename = model_name + '.incon',
+        silent = True)
+
+jsondata = dat.json(geo, mesh_filename, incons = inc, bdy_incons = inc)
+json.dump(jsondata, file(model_name + 'b.json', 'w'), indent = 2)
+                  
+os.chdir(orig_dir)
---
+++ test/benchmark/model_intercomparison_study/problem5/test_problem5.py
@@ -0,0 +1,200 @@
+"""
+Model Intercomparison Study problem 5
+"""
+
+import os
+
+from credo.systest import SciBenchmarkTest
+from credo.systest import FieldWithinTolTC
+from credo.systest import HistoryWithinTolTC
+
+from credo.jobrunner import SimpleJobRunner
+from credo.modelresult import ModelResult
+from credo.t2model import T2ModelRun, T2ModelResult
+from credo.supermodel import SuperModelRun
+
+import credo.reporting.standardReports as sReps
+from credo.reporting import getGenerators
+
+from mulgrids import mulgrid
+import matplotlib.pyplot as plt
+import numpy as np
+
+from docutils.core import publish_file
+
+class DigitisedHistoryResult(ModelResult):
+    """Digitised results for a field at a single cell."""
+    def __init__(self, modelName, fileName, field, cellIndex, ordering_map=None,
+                 fieldname_map=None):
+        from os.path import dirname
+        super(DigitisedHistoryResult, self).__init__(modelName, dirname(fileName),
+                                            ordering_map=ordering_map,
+                                            fieldname_map=fieldname_map)
+        self.field = field
+        self.cellIndex = cellIndex
+        self.data = np.loadtxt(fileName)
+    def _getTimes(self): return self.data[:, 0]
+    def _getFieldHistoryAtCell(self, field, cellIndex):
+        if field == self.field and cellIndex == self.cellIndex:
+            return self.data[:, 1]
+        else: return None
+
+model_name = 'problem5'
+
+AUT2_FIELDMAP = {
+    'Pressure': 'Pressure',
+    'Temperature': 'Temperature',
+    'Vapour saturation': 'Vapour saturation',
+}
+SUPER_FIELDMAP = {
+    'Pressure': 'fluid_pressure',
+    'Temperature': 'fluid_temperature',
+    'Vapour saturation': 'fluid_vapour_saturation',
+}
+
+model_dir = './run'
+data_dir = './data'
+t2geo_filename = os.path.join(model_dir, 'g' + model_name + '.dat')
+
+num_procs = 1
+
+run_names = ['a', 'b']
+test_fields = ["Pressure", "Temperature", "Vapour saturation"]
+plot_fields = test_fields
+digitised_test_fields = {'production': ["Pressure", "Temperature"],
+                         'injection': ["Pressure"]}
+digitised_simulators = ["LBL", "S-Cubed"]    
+
+geo = mulgrid(t2geo_filename)
+map_out_atm = range(geo.num_atmosphere_blocks, geo.num_blocks)
+
+problem5_test = SciBenchmarkTest(model_name + "_test", nproc = num_procs)
+problem5_test.description = """Model Intercomparison Study problem 5"""
+
+def geo_pos_block_index(pos):
+    # returns index of bottom layer block containing (x,y) pos
+    col = geo.column_containing_point(pos)
+    blkname = geo.block_name(geo.layerlist[-1].name, col.name)
+    return geo.block_name_index[blkname]
+
+obs_points = ["production", "injection"]
+obs_positions = {"production": np.array([62.5, 62.5]),
+             "injection": np.array([162.5, 137.5])}
+obs_cell_index = dict([(obspt, geo_pos_block_index(obs_positions[obspt]))
+                       for obspt in obs_points])
+
+for run_index, run_name in enumerate(run_names):
+
+    run_base_name = model_name + run_name
+    run_filename = run_base_name + '.json'
+    model_run = SuperModelRun(run_name, run_filename,
+                              fieldname_map = SUPER_FIELDMAP,
+                              simulator = 'supermodel',
+                              basePath = os.path.realpath(model_dir))
+    model_run.jobParams['nproc'] = num_procs
+    problem5_test.mSuite.addRun(model_run, run_name)
+
+problem5_test.setupEmptyTestCompsList()
+digitised_result = {}
+
+for run_index, run_name in enumerate(run_names):
+
+    run_base_name = model_name + run_name
+    run_filename = os.path.join(model_dir, run_base_name + ".listing")
+    AUTOUGH2_result = T2ModelResult("AUTOUGH2", run_filename,
+                                     geo_filename = t2geo_filename,
+                                     ordering_map = map_out_atm,
+                                     fieldname_map = AUT2_FIELDMAP)
+    for obspt in obs_points:
+        blk_index = obs_cell_index[obspt]
+        problem5_test.addTestComp(run_index, "AUTOUGH2 " + obspt + " well",
+                              HistoryWithinTolTC(fieldsToTest = test_fields,
+                                                 defFieldTol = 1.e-3,
+                                                 expected = AUTOUGH2_result,
+                                                 testCellIndex = blk_index))
+
+        for field_name in digitised_test_fields[obspt]:
+            for sim in digitised_simulators:
+                data_filename = '_'.join((model_name + run_name, obspt, field_name, sim))
+                data_filename = os.path.join(data_dir, data_filename.lower() + '.dat')
+                result = DigitisedHistoryResult(sim, data_filename,
+                                                       field_name, blk_index,
+                                                       ordering_map = map_out_atm,
+                                                       fieldname_map = AUT2_FIELDMAP)
+                digitised_result[run_name, obspt, field_name, sim] = result
+                problem5_test.addTestComp(run_index, ' '.join((sim, field_name, obspt + ' well')),
+                                          HistoryWithinTolTC(fieldsToTest = [field_name],
+                                                             defFieldTol = 1.e-2,
+                                                             expected = result,
+                                                             testCellIndex = obs_cell_index[obspt]))
+
+jrunner = SimpleJobRunner(mpi = True)
+testResult, mResults = problem5_test.runTest(jrunner, createReports = True)
+
+# plots:
+elevations = [layer.centre for layer in geo.layerlist[1:]]
+scale = {"Pressure": 1.e5, "Temperature": 1.}
+unit = {"Pressure": "bar", "Temperature": "$^{\circ}$C"}
+symbol = {"LBL": 's', "S-Cubed": 'o'}
+yr = 365. * 24. * 60. * 60.
+
+for run_index, run_name in enumerate(run_names):
+    for obspt in obs_points:
+
+        # plot time history results at well:
+        tc_name = "AUTOUGH2 " + obspt + " well"
+        blk_index = obs_cell_index[obspt]
+
+        for field_name in digitised_test_fields[obspt]:
+
+            t = problem5_test.testComps[run_index][tc_name].times
+            var = problem5_test.mSuite.resultsList[run_index].getFieldHistoryAtCell(field_name, blk_index)
+            plt.plot(t / yr, var / scale[field_name], '-', label = 'supermodel')
+            for sim in digitised_simulators:
+                result = digitised_result[run_name, obspt, field_name, sim]
+                t = result.getTimes()
+                var = result.getFieldHistoryAtCell(field_name, blk_index)
+                plt.plot(t / yr, var / scale[field_name], symbol[sim], label = sim)
+            plt.xlabel('time (years)')
+            plt.ylabel(field_name + ' (' + unit[field_name] + ')')
+            plt.legend()
+            plt.title(' '.join((model_name + run_name, field_name.lower(),
+                                'results at', obspt, 'well')))
+            img_filename_base = '_'.join((model_name, run_name, obspt, field_name))
+            img_filename_base = img_filename_base.replace(' ', '_')
+            img_filename = os.path.join(problem5_test.mSuite.runs[run_index].basePath,
+                                        problem5_test.mSuite.outputPathBase,
+                                        img_filename_base + '.png')
+            plt.savefig(img_filename)
+            plt.clf()
+            problem5_test.mSuite.analysisImages.append(img_filename)
+
+        t = problem5_test.testComps[run_index][tc_name].times
+        for field_name in plot_fields:
+            var = np.array(problem5_test.testComps[run_index][tc_name].fieldErrors[field_name])
+            plt.plot(t / yr, var, '-o')
+            plt.xlabel('time (years)')
+            plt.ylabel(field_name + ' error')
+            plt.title(' '.join((model_name + run_name, 'comparison with AUTOUGH2 at',
+                               obspt, 'well')))
+            img_filename_base = '_'.join((model_name, run_name, tc_name, 'error', field_name))
+            img_filename_base = img_filename_base.replace(' ', '_')
+            img_filename = os.path.join(problem5_test.mSuite.runs[run_index].basePath,
+                                        problem5_test.mSuite.outputPathBase,
+                                        img_filename_base + '.png')
+            plt.savefig(img_filename)
+            plt.clf()
+            problem5_test.mSuite.analysisImages.append(img_filename)
+
+# generate report:
+
+for rGen in getGenerators(["RST"], problem5_test.outputPathBase):
+    report_filename = os.path.join(problem5_test.outputPathBase,
+                     "%s-report.%s" % (problem5_test.testName, rGen.stdExt))
+    sReps.makeSciBenchReport(problem5_test, mResults, rGen, report_filename)
+    html_filename = os.path.join(problem5_test.outputPathBase,
+                     "%s-report.%s" % (problem5_test.testName, 'html'))
+    html = publish_file(source_path = report_filename,
+                        destination_path = html_filename,
+                        writer_name = "html")
+
