687b2d91749e463a8fcb9900ea38cbf68f464e39
Adrian Croucher <a.croucher@auckland.ac.nz>
2017-02-02 15:57:14 +1300

    Added unit test for 2D radial geometry

--- test/unit/src/mesh_test.F90
+++ test/unit/src/mesh_test.F90
@@ -13,9 +13,10 @@ module mesh_test
   implicit none
   private
 
-public :: test_mesh_init, test_2d_geometry
+  public :: test_mesh_init, test_2d_cartesian_geometry, &
+       test_2d_radial_geometry
 
-PetscReal, parameter :: tol = 1.e-6_dp
+  PetscReal, parameter :: tol = 1.e-6_dp
 
 contains
 
@@ -128,7 +129,7 @@ contains
 
 !------------------------------------------------------------------------
 
-  subroutine test_2d_geometry
+  subroutine test_2d_cartesian_geometry
     ! Test 2D Cartesian volumes and areas
 
     use fson_mpi_module
@@ -150,7 +151,6 @@ contains
     PetscBool :: volumes_OK, areas_OK
     PetscReal, parameter :: expected_cell_vol = 62500._dp
     PetscReal, parameter :: expected_face_area = 2500._dp
-    PetscReal, parameter :: tol = 1.e-6_dp
 
     call MPI_COMM_RANK(PETSC_COMM_WORLD, rank, ierr)
     primary = ["Pressure   ", "Temperature"]
@@ -209,7 +209,105 @@ contains
 
     call mesh%destroy()
 
-  end subroutine test_2d_geometry
+  end subroutine test_2d_cartesian_geometry
+
+!------------------------------------------------------------------------
+
+  subroutine test_2d_radial_geometry
+    ! Test 2D radial volumes and areas
+
+    use fson_mpi_module
+    use cell_module
+    use face_module
+    use dm_utils_module, only: section_offset, local_vec_section
+    use utils_module, only: pi
+
+    type(fson_value), pointer :: json
+    type(mesh_type) :: mesh
+    character(len = 11), allocatable :: primary(:)
+    PetscReal, pointer, contiguous :: cell_geom_array(:), face_geom_array(:)
+    PetscSection :: cell_geom_section, face_geom_section
+    PetscInt :: c, offset, f
+    type(cell_type) :: cell
+    type(face_type) :: face
+    PetscErrorCode :: ierr
+    PetscMPIInt :: rank
+    character(50) :: msg
+    PetscBool :: volumes_OK, areas_OK
+    PetscReal :: r, r1, r2, l, expected_cell_vol, expected_face_area
+    PetscBool :: vertical
+    PetscReal, parameter :: dr = 300._dp / 12, dy = 200._dp / 8
+
+    call MPI_COMM_RANK(PETSC_COMM_WORLD, rank, ierr)
+    primary = ["Pressure   ", "Temperature"]
+
+    json => fson_parse_mpi(str = '{"mesh": {' // &
+         '"filename": "data/mesh/2D.msh",' // &
+         '"radial": true}}')
+    call mesh%init(json)
+    call fson_destroy_mpi(json)
+    call mesh%configure(primary)
+
+    call local_vec_section(mesh%cell_geom, cell_geom_section)
+    call VecGetArrayReadF90(mesh%cell_geom, cell_geom_array, ierr)
+    CHKERRQ(ierr)
+
+    volumes_OK = PETSC_TRUE
+
+    do c = mesh%start_cell, mesh%end_cell - 1
+       if (mesh%ghost_cell(c) < 0) then
+          call section_offset(cell_geom_section, c, offset, &
+               ierr); CHKERRQ(ierr)
+          call cell%assign_geometry(cell_geom_array, offset)
+          r = cell%centroid(1)
+          r1 = r - 0.5_dp * dr
+          r2 = r + 0.5_dp * dr
+          expected_cell_vol = pi * (r2 * r2 - r1 * r1) * dy
+          volumes_OK = (volumes_OK .and. &
+               abs(cell%volume - expected_cell_vol) <= tol)
+       end if
+    end do
+
+    write(msg, '(a, i3)') 'cell volumes on proc', rank
+    call assert_true(volumes_OK, msg)
+
+    call VecRestoreArrayReadF90(mesh%cell_geom, cell_geom_array, ierr)
+    CHKERRQ(ierr)
+
+    call local_vec_section(mesh%face_geom, face_geom_section)
+    call VecGetArrayReadF90(mesh%face_geom, face_geom_array, ierr)
+    CHKERRQ(ierr)
+    call face%init()
+    areas_OK = PETSC_TRUE
+
+    do f = mesh%start_face, mesh%end_face - 1
+       if (mesh%ghost_face(f) < 0) then
+          call section_offset(face_geom_section, f, offset, &
+               ierr); CHKERRQ(ierr)
+          call face%assign_geometry(face_geom_array, offset)
+          r = face%centroid(1)
+          vertical = (abs(face%normal(2)) > tol)
+          if (vertical) then
+             l = dr
+          else
+             l = dy
+          end if
+          expected_face_area = 2._dp * pi * r * l
+          areas_OK = (areas_OK .and. &
+               abs(face%area - expected_face_area) <= tol)
+       end if
+    end do
+
+    write(msg, '(a, i3)') 'face areas on proc', rank
+    call assert_true(areas_OK, msg)
+
+    call VecRestoreArrayReadF90(mesh%face_geom, face_geom_array, ierr)
+    CHKERRQ(ierr)
+    call face%destroy()
+
+    call mesh%destroy()
+
+  end subroutine test_2d_radial_geometry
 
 !------------------------------------------------------------------------
 
