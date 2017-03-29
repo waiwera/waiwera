b18e6cbacc4b994801e5480a001e98dc42bdb12a
Adrian Croucher <a.croucher@auckland.ac.nz>
2017-03-29 17:04:03 +1300

    Add CO2 energy of solution unit tests

--- test/unit/src/ncg_co2_thermodynamics_test.F90
+++ test/unit/src/ncg_co2_thermodynamics_test.F90
@@ -12,7 +12,7 @@ module ncg_co2_thermodynamics_test
   implicit none
   private
 
-  public :: test_ncg_co2_henrys_constant
+  public :: test_ncg_co2_henrys_constant, test_ncg_co2_energy_solution
 
 contains
 
@@ -62,6 +62,50 @@ contains
     end if
 
   end subroutine test_ncg_co2_henrys_constant
+
+!------------------------------------------------------------------------
+
+  subroutine test_ncg_co2_energy_solution
+
+    type(ncg_co2_thermodynamics_type) :: gas
+    PetscReal :: temperature, expected, hs
+    PetscErrorCode :: err
+    character(33) :: s = "CO2 energy of solution, temperature"
+    PetscMPIInt :: rank
+    PetscInt :: ierr
+    PetscReal, parameter :: tol = 1.e-4_dp
+
+    call MPI_COMM_RANK(PETSC_COMM_WORLD, rank, ierr)
+
+    if (rank == 0) then
+
+       temperature = 20._dp
+       expected = -461218.6464_dp
+       call gas%energy_solution(temperature, hs, err)
+       call assert_equals(0, err, s // " 20 deg C error")
+       call assert_equals(expected, hs, tol, s // " 20 deg C")
+
+       temperature = 100._dp
+       expected = -180238.0_dp
+       call gas%energy_solution(temperature, hs, err)
+       call assert_equals(0, err, s // " 100 deg C error")
+       call assert_equals(expected, hs, tol, s // " 100 deg C")
+
+       temperature = 240._dp
+       expected = 180225.4096_dp
+       call gas%energy_solution(temperature, hs, err)
+       call assert_equals(0, err, s // " 240 deg C error")
+       call assert_equals(expected, hs, tol, s // " 240 deg C")
+
+       temperature = 300._dp
+       expected = 492442.0_dp
+       call gas%energy_solution(temperature, hs, err)
+       call assert_equals(0, err, s // " 300 deg C error")
+       call assert_equals(expected, hs, tol, s // " 300 deg C")
+
+    end if
+
+  end subroutine test_ncg_co2_energy_solution
 
 !------------------------------------------------------------------------
 
