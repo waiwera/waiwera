f7d59e5d06910a841002c9c0e5fd1b1b5d11de5f
Adrian Croucher <a.croucher@auckland.ac.nz>
2017-03-30 13:16:21 +1300

    Add CO2 thermodynamics properties() unit tests

--- test/unit/src/ncg_co2_thermodynamics_test.F90
+++ test/unit/src/ncg_co2_thermodynamics_test.F90
@@ -13,7 +13,7 @@ module ncg_co2_thermodynamics_test
   private
 
   public :: test_ncg_co2_henrys_constant, test_ncg_co2_energy_solution, &
-       test_ncg_co2_viscosity
+       test_ncg_co2_viscosity, test_ncg_co2_properties
 
 contains
 
@@ -165,6 +165,58 @@ contains
     end if
 
   end subroutine test_ncg_co2_viscosity
+
+!------------------------------------------------------------------------
+
+  subroutine test_ncg_co2_properties
+    ! CO2 properties
+
+    type(ncg_co2_thermodynamics_type) :: gas
+    PetscMPIInt :: rank
+    PetscInt :: ierr, err, i
+    PetscReal :: props(2), xg
+    character(40) :: s
+    PetscReal, parameter :: water_density = 998._dp
+    PetscReal, parameter :: htol = 1.e-3_dp, dtol = 1.e-7_dp
+    PetscInt, parameter :: num_cases = 14, phase = 2
+    PetscReal, parameter :: data(4, num_cases) = reshape([ &
+         0.0_dp, 20.0_dp, 17140.18077231938_dp, 0.0_dp, &
+         100000.0_dp, 20.0_dp, 16142.247883091828_dp, 1.8142044368713437_dp, &
+         0.0_dp, 100.0_dp, 87450.99131436742_dp, 0.0_dp, &
+         100000.0_dp, 100.0_dp, 87004.524163092_dp, 1.4213754811567743_dp, &
+         4000000.0_dp, 100.0_dp, 64355.3813832885_dp, 62.608990505735434_dp, &
+         9000000.0_dp, 100.0_dp, 20379.357776952613_dp, 184.7959892299282_dp, &
+         0.0_dp, 240.0_dp, 223594.37705727902_dp, 0.0_dp, &
+         100000.0_dp, 240.0_dp, 223439.99865083068_dp, 1.0324489144812645_dp, &
+         4000000.0_dp, 240.0_dp, 215608.4290498441_dp, 42.27375154306431_dp, &
+         9000000.0_dp, 240.0_dp, 200402.49860929986_dp, 100.70459422220841_dp, &
+         0.0_dp, 300.0_dp, 286380.4950504236_dp, 0.0_dp, &
+         100000.0_dp, 300.0_dp, 286273.71092985675_dp, 0.9242369906584087_dp, &
+         4000000.0_dp, 300.0_dp, 280856.58497462136_dp, 37.5055455044134_dp, &
+         9000000.0_dp, 300.0_dp, 270338.58607276645_dp, 87.3627658128452_dp &
+         ], [4, num_cases])
+
+    call gas%init()
+
+    call MPI_COMM_RANK(PETSC_COMM_WORLD, rank, ierr)
+
+    if (rank == 0) then
+
+       do i = 1, num_cases
+          associate(pc => data(1, i), t => data(2, i), &
+               expected_h => data(3, i), expected_d => data(4, i), &
+               d => props(1), h => props(2))
+            call gas%properties(pc, t, phase, water_density, props, xg, err)
+            write(s, '(a, e8.3, a, f6.2)') 'pc = ', pc, ', t = ', t
+            call assert_equals(0, err, trim(s) // ' error')
+            call assert_equals(expected_h, h, htol, trim(s) // ' enthalpy')
+            call assert_equals(expected_d, d, dtol, trim(s) // ' density')
+          end associate
+       end do
+
+    end if
+
+  end subroutine test_ncg_co2_properties
 
 !------------------------------------------------------------------------
 
