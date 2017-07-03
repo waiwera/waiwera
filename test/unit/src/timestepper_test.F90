ddf6270406da6a9e9bd52164cc5c81eefe64a6af
Adrian Croucher <a.croucher@auckland.ac.nz>
2017-07-03 13:34:08 +1200

    Need to assign mesh%dm pointer in timestepper unit tests

--- test/unit/src/timestepper_test.F90
+++ test/unit/src/timestepper_test.F90
@@ -169,6 +169,7 @@ contains
     self%dof = 1
     self%stencil = 0
 
+    call self%mesh%assign_dm(self%mesh%original_dm)
     call DMDACreate1d(PETSC_COMM_WORLD, DM_BOUNDARY_NONE, self%dim, self%dof, &
          self%stencil, PETSC_NULL_INTEGER, self%mesh%dm, ierr); CHKERRQ(ierr)
     call DMSetUp(self%mesh%dm, ierr); CHKERRQ(ierr)
@@ -501,6 +502,7 @@ contains
     dx = self%L / (self%dim - 1._dp)
     self%a = 1._dp / (dx*dx)
 
+    call self%mesh%assign_dm(self%mesh%original_dm)
     call DMDACreate1d(PETSC_COMM_WORLD, DM_BOUNDARY_GHOSTED, self%dim-2, &
          self%dof, self%stencil, PETSC_NULL_INTEGER, self%mesh%dm, &
          ierr); CHKERRQ(ierr)
