cf5bf0f2cad575cf0ef66d97b41d94497510be93
Adrian Croucher <a.croucher@auckland.ac.nz>
2017-07-04 17:30:01 +1200

    Add zone box type

--- src/zone.F90
+++ src/zone.F90
@@ -45,12 +45,23 @@ module zone_module
 
   type, public, extends(zone_type) :: zone_cell_array_type
      !! Zone defined by an array of global cell indices.
+     private
      PetscInt, allocatable, public :: cells(:)
    contains
      procedure, public :: init => zone_cell_array_init
      procedure, public :: destroy => zone_cell_array_destroy
      procedure, public :: find_cells => zone_cell_array_find_cells
   end type zone_cell_array_type
+
+  type, public, extends(zone_type) :: zone_box_type
+     !! Zone defined by coordinate ranges.
+     private
+     PetscReal, public :: coord_range(2, 3)
+     PetscBool, public :: coord_specified(3)
+   contains
+     procedure, public :: init => zone_box_init
+     procedure, public :: find_cells => zone_box_find_cells
+  end type zone_box_type
 
   public :: get_zone_type
 
@@ -232,6 +243,107 @@ contains
     end do
 
   end subroutine zone_cell_array_find_cells
+
+!------------------------------------------------------------------------
+! zone_box_type
+!------------------------------------------------------------------------
+
+  subroutine zone_box_init(self, index, json)
+    !! Initialise box zone.
+
+    class(zone_box_type), intent(in out) :: self
+    PetscInt, intent(in) :: index
+    type(fson_value), pointer, intent(in) :: json
+
+    call self%zone_type%init(index, json)
+    self%coord_specified = PETSC_FALSE
+
+    call set_coords(1, "x")
+    call set_coords(1, "r")
+    call set_coords(2, "y")
+    call set_coords(3, "z")
+
+  contains
+
+    subroutine set_coords(index, name)
+
+      PetscInt, intent(in) :: index
+      character(*), intent(in) :: name
+      ! Locals:
+      PetscReal, allocatable :: r(:)
+
+      if (fson_has_mpi(json, name)) then
+         self%coord_specified(index) = PETSC_TRUE
+         call fson_get_mpi(json, name, val = r)
+         self%coord_range(:, index) = r
+      end if
+
+    end subroutine set_coords
+
+  end subroutine zone_box_init
+
+!------------------------------------------------------------------------
+
+  subroutine zone_box_find_cells(self, dm, cell_geometry, err)
+    !! Find cells in a zone from specified coordinate ranges.
+
+    use dm_utils_module, only: local_vec_section, section_offset
+    use cell_module
+
+    class(zone_box_type), intent(in out) :: self
+    DM, intent(in out) :: dm
+    Vec, intent(in) :: cell_geometry
+    PetscErrorCode, intent(out) :: err
+    ! Locals:
+    PetscInt :: dim, ghost, i, c, offset
+    DMLabel :: ghost_label
+    PetscInt :: start_cell, end_cell
+    PetscSection :: section
+    PetscReal, contiguous, pointer :: cell_geom_array(:)
+    type(cell_type) :: cell
+    PetscBool :: found(3)
+    PetscErrorCode :: ierr
+
+    err = 0
+
+    call DMGetDimension(dm, dim, ierr); CHKERRQ(ierr)
+    call DMPlexGetHeightStratum(dm, 0, start_cell, end_cell, ierr)
+    CHKERRQ(ierr)
+    call DMGetLabel(dm, "ghost", ghost_label, ierr); CHKERRQ(ierr)
+    call local_vec_section(cell_geometry, section)
+    call VecGetArrayReadF90(cell_geometry, cell_geom_array, ierr); CHKERRQ(ierr)
+
+    do c = start_cell, end_cell - 1
+
+       call DMLabelGetValue(ghost_label, c, ghost, ierr); CHKERRQ(ierr)
+       if (ghost < 0) then
+
+          call section_offset(section, c, offset, ierr); CHKERRQ(ierr)
+          call cell%assign_geometry(cell_geom_array, offset)
+          found = PETSC_TRUE
+
+          do i = 1, dim
+             if (self%coord_specified(i)) then
+                found(i) = &
+                     (self%coord_range(1, i) <= cell%centroid(i)) .and. &
+                     (cell%centroid(i) <= self%coord_range(2, i))
+             else
+                found(i) = PETSC_TRUE
+             end if
+          end do
+
+          if (all(found)) then
+             call DMSetLabelValue(dm, zone_label_name, c, self%index, &
+                  ierr); CHKERRQ(ierr)
+          end if
+
+       end if
+    end do
+
+    call VecRestoreArrayReadF90(cell_geometry, cell_geom_array, ierr)
+    CHKERRQ(ierr)
+
+  end subroutine zone_box_find_cells
 
 !------------------------------------------------------------------------  
 
