c28fd1236f687fed915524a92bff1ef3b8c56ccd
Adrian Croucher <a.croucher@auckland.ac.nz>
2018-09-13 16:03:54 +1200

    Add flow rate and enthalpy tests for deliverability source benchmark tests (and change plots to flow rate)

--- test/benchmark/source/deliverability/test_deliverability.py
+++ test/benchmark/source/deliverability/test_deliverability.py
@@ -27,6 +27,8 @@ WAIWERA_FIELDMAP = {
     'Pressure': 'fluid_pressure',
     'Temperature': 'fluid_temperature',
     'Vapour saturation': 'fluid_vapour_saturation',
+    'Generation rate': 'source_rate',
+    'Enthalpy': 'source_enthalpy'
 }
 
 model_dir = './run'
@@ -36,13 +38,19 @@ num_procs = 1
 
 run_names = ['delv', 'delg_flow', 'delg_pi_table', 'delg_pwb_table', 'delg_limit', 'delt', 'delw']
 test_fields = ["Pressure", "Temperature", "Vapour saturation"]
+test_source_fields = ["Generation rate", "Enthalpy"]
 plot_fields = ["Pressure", "Vapour saturation"]
+plot_source_fields = ["Generation rate"]
+scale = {"Generation rate": 1.}
+unit = {"Generation rate": "kg/s"}
 
 geo = mulgrid(t2geo_filename)
 map_out_atm = range(geo.num_atmosphere_blocks, geo.num_blocks)
 
 deliverability_test = SciBenchmarkTest("Deliverability", nproc = num_procs)
 deliverability_test.description = """Tests sources on deliverability"""
+
+source_index = 0
 
 for run_index, run_name in enumerate(run_names):
 
@@ -57,23 +65,29 @@ for run_index, run_name in enumerate(run_names):
 
 deliverability_test.setupEmptyTestCompsList()
 
+reference_result = {}
 for run_index, run_name in enumerate(run_names):
 
     run_base_name = '_'.join((model_name, run_name))
     run_filename = os.path.join(model_dir, run_base_name + ".listing")
-    reference_result = T2ModelResult("aut2", run_filename,
+    reference_result[run_name] = T2ModelResult("aut2", run_filename,
                                      geo_filename = t2geo_filename,
                                      ordering_map = map_out_atm)
     deliverability_test.addTestComp(run_index, "final errors",
                                         FieldWithinTolTC(fieldsToTest = test_fields,
                                                          defFieldTol = 5.e-3,
-                                                         expected = reference_result,
+                                                         expected = reference_result[run_name],
                                                          testOutputIndex = -1))
     deliverability_test.addTestComp(run_index, "time history",
                                     HistoryWithinTolTC(fieldsToTest = test_fields,
                                                        defFieldTol = 1.e-2,
-                                                       expected = reference_result,
+                                                       expected = reference_result[run_name],
                                                        testCellIndex = 4))
+    deliverability_test.addTestComp(run_index, "source",
+                                    HistoryWithinTolTC(fieldsToTest = test_source_fields,
+                                                       defFieldTol = 1.e-2,
+                                                       expected = reference_result[run_name],
+                                                       testSourceIndex = source_index))
     
 jrunner = SimpleJobRunner(mpi = True)
 testResult, mResults = deliverability_test.runTest(jrunner, createReports = True)
@@ -83,33 +97,21 @@ x = [col.centre[0] for col in geo.columnlist]
 
 for run_index, run_name in enumerate(run_names):
 
-    run_base_name = '_'.join((model_name, run_name))
-    tc_name = "time history"
-    t = deliverability_test.testComps[run_index][tc_name].times
-    for field_name in plot_fields:
-        var = np.array(deliverability_test.testComps[run_index][tc_name].fieldErrors[field_name])
-        plt.semilogx(t, var, '-o')
-        plt.xlabel('time (s)')
-        plt.ylabel(field_name + ' error')
-        plt.title(run_name + ' ' + tc_name)
-        img_filename_base = '_'.join((run_base_name, tc_name, 'error', field_name))
-        img_filename_base = img_filename_base.replace(' ', '_')
-        img_filename = os.path.join(deliverability_test.mSuite.runs[run_index].basePath,
-                                    deliverability_test.mSuite.outputPathBase,
-                                    img_filename_base + '.png')
-        plt.tight_layout(pad = 3.)
-        plt.savefig(img_filename)
-        plt.clf()
-        deliverability_test.mSuite.analysisImages.append(img_filename)
+    tc_name = "source"
+    for field_name in plot_source_fields:
 
-    tc_name = "final errors"
-    for field_name in plot_fields:
-        var = np.array(deliverability_test.testComps[run_index][tc_name].fieldErrors[field_name])
-        plt.plot(x, var, 'o-')
-        plt.xlabel('x (m)')
-        plt.ylabel(field_name + ' error')
-        plt.title(run_name + ' ' + tc_name)
-        img_filename_base = '_'.join((run_base_name, tc_name, field_name))
+        t, var = deliverability_test.mSuite.resultsList[run_index].\
+                 getFieldHistoryAtSource(field_name, source_index)
+        plt.plot(t, var / scale[field_name], '-', label = 'Waiwera')
+
+        t, var = reference_result[run_name].getFieldHistoryAtSource(field_name, source_index)
+        plt.plot(t, var / scale[field_name], '+', label = 'AUTOUGH2')
+
+        plt.xlabel('time (s)')
+        plt.ylabel(field_name + ' (' + unit[field_name] + ')')
+        plt.legend(loc = 'best')
+        plt.title(' '.join((run_name, field_name.lower())))
+        img_filename_base = '_'.join((model_name, run_name, tc_name, field_name))
         img_filename_base = img_filename_base.replace(' ', '_')
         img_filename = os.path.join(deliverability_test.mSuite.runs[run_index].basePath,
                                     deliverability_test.mSuite.outputPathBase,
