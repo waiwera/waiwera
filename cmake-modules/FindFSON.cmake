39d79f94ad95abeea76837081a1be91d15344645
Adrian Croucher <a.croucher@auckland.ac.nz>
2018-09-28 14:35:00 +1200

    FindFSON.cmake looks for FSON in $FSON_DIR environment variable

--- cmake-modules/FindFSON.cmake
+++ cmake-modules/FindFSON.cmake
@@ -3,26 +3,18 @@
 # sets the following variables:
 # FSON_FOUND   -- the system has FSON
 # FSON_MODULES -- path to FSON Fortran modules
-# FSON_LIBRARY -- the FSON library (link these to use FSON)
+# FSON_LIBRARY -- the FSON library (link this to use FSON)
 
-find_path(FSON_MODULES build/fson.mod
-  HINTS /home/acro018/software/fson)
+find_path(FSON_MODULES fson.mod
+  HINTS $ENV{FSON_DIR}/build)
 
-find_library(FSON_LIBRARY dist/libfson.so
-  HINTS /home/acro018/software/fson)
+find_library(FSON_LIBRARY libfson.so
+  HINTS $ENV{FSON_DIR}/dist)
 
-if (FSON_LIBRARY)
-  if ((FSON_MODULES) STREQUAL (FSON_LIBRARY))
-    set(FSON_FOUND "TRUE")
-  else ()
-    set(FSON_FOUND "FALSE")
-  endif ()
+if ((FSON_LIBRARY) AND (FSON_MODULES))
+  set(FSON_FOUND "TRUE")
+  message(STATUS "Found FSON: " ${FSON_LIBRARY})
 else ()
   set(FSON_FOUND "FALSE")
-endif ()
-
-if (FSON_FOUND STREQUAL "FALSE")
-  message (STATUS "Could not find FSON library")
-else ()
-  message (STATUS "Found FSON: " ${FSON_LIBRARY})
+  message(STATUS "FSON not found")
 endif ()
