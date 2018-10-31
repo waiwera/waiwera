0f6a1c3c8da0a7eaece55c2770061471f33d5f2b
Tim Harton <t.harton@auckland.ac.nz>
2018-10-31 13:00:37 +1300

    try install openmpi via download

--- ansible/files/opt_paths.sh
+++ ansible/files/opt_paths.sh
@@ -1,6 +1,6 @@
 
 export PETSC_DIR=/opt/app/petsc
 export PETSC_ARCH=arch-linux2-c-debug
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/app/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/app/lib:/opt/app/openmpi/lib
 export PYTHONPATH=/opt/app/PyTOUGH:/opt/app/credo2:/opt/app/supermodels-test/utils
-export PATH=$PATH:/opt/app/bin:/opt/app/supermodels-test/dist
+export PATH=$PATH:/opt/app/bin:/opt/app/supermodels-test/dist:/opt/app/openmpi/bin
