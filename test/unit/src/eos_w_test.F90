38d57c97f312dd7fcff2085ff60dead6c1191952
Adrian Croucher <a.croucher@auckland.ac.nz>
2018-11-13 13:50:43 +1300

    Convert eos_w unit tests to Zofu

--- test/unit/src/eos_w_test.F90
+++ test/unit/src/eos_w_test.F90
@@ -6,7 +6,7 @@ module eos_w_test
 
   use petsc
   use kinds_module
-  use fruit
+  use zofu
   use fluid_module
   use rock_module
   use relative_permeability_module
@@ -18,16 +18,53 @@ module eos_w_test
   implicit none
   private
 
+  public :: setup, teardown, setup_test
   public :: test_eos_w_fluid_properties
 
 contains
 
 !------------------------------------------------------------------------
 
-  subroutine test_eos_w_fluid_properties
+  subroutine setup()
+
+    use profiling_module, only: init_profiling
+
+    ! Locals:
+    PetscErrorCode :: ierr
+
+    call PetscInitialize(PETSC_NULL_CHARACTER, ierr); CHKERRQ(ierr)
+    call init_profiling()
+
+  end subroutine setup
+
+!------------------------------------------------------------------------
+
+  subroutine teardown()
+
+    PetscErrorCode :: ierr
+
+    call PetscFinalize(ierr); CHKERRQ(ierr)
+
+  end subroutine teardown
+
+!------------------------------------------------------------------------
+
+  subroutine setup_test(test)
+
+    class(unit_test_type), intent(in out) :: test
+
+    test%tolerance = 1.e-8
+
+  end subroutine setup_test
+
+!------------------------------------------------------------------------
+
+  subroutine test_eos_w_fluid_properties(test)
 
     ! eos_w fluid_properties() test
 
+    class(unit_test_type), intent(in out) :: test
+    ! Locals:
     type(fluid_type) :: fluid
     type(rock_type) :: rock
     PetscInt,  parameter :: offset = 1, region = 1, phase_composition = b'01'
@@ -38,7 +75,7 @@ contains
     type(fson_value), pointer :: json
     character(40) :: json_str = '{"eos":{"temperature": 20.0}}'
     PetscErrorCode :: err
-    PetscReal, parameter :: pressure = 1.e5_dp, tol = 1.e-8_dp
+    PetscReal, parameter :: pressure = 1.e5_dp
     PetscReal, parameter :: expected_density = 998.20548637769673_dp
     PetscReal, parameter :: expected_internal_energy = 83911.631393167205_dp
     PetscReal, parameter :: expected_specific_enthalpy = 84011.811167136271_dp
@@ -67,24 +104,24 @@ contains
     call MPI_COMM_RANK(PETSC_COMM_WORLD, rank, ierr)
     if (rank == 0) then
 
-       call assert_equals(pressure, fluid%pressure, tol, "Pressure")
-       call assert_equals(eos%temperature, fluid%temperature, tol, "Temperature")
-       call assert_equals(phase_composition, nint(fluid%phase_composition), "Phase composition")
+       call test%assert(pressure, fluid%pressure, "Pressure")
+       call test%assert(eos%temperature, fluid%temperature, "Temperature")
+       call test%assert(phase_composition, nint(fluid%phase_composition), "Phase composition")
 
-       call assert_equals(expected_density, fluid%phase(1)%density, &
-            tol, "Density")
-       call assert_equals(expected_internal_energy, fluid%phase(1)%internal_energy, &
-            tol, "Internal energy")
-       call assert_equals(expected_specific_enthalpy, fluid%phase(1)%specific_enthalpy, &
-            tol, "Specific enthalpy")
-       call assert_equals(expected_viscosity, fluid%phase(1)%viscosity, &
-            tol, "Viscosity")
+       call test%assert(expected_density, fluid%phase(1)%density, &
+            "Density")
+       call test%assert(expected_internal_energy, fluid%phase(1)%internal_energy, &
+            "Internal energy")
+       call test%assert(expected_specific_enthalpy, fluid%phase(1)%specific_enthalpy, &
+            "Specific enthalpy")
+       call test%assert(expected_viscosity, fluid%phase(1)%viscosity, &
+            "Viscosity")
 
-       call assert_equals(1._dp, fluid%phase(1)%saturation, tol, "Saturation")
-       call assert_equals(1._dp, fluid%phase(1)%relative_permeability, tol, "Relative permeability")
-       call assert_equals(1._dp, fluid%phase(1)%mass_fraction(1), tol, "Mass fraction")
+       call test%assert(1._dp, fluid%phase(1)%saturation, "Saturation")
+       call test%assert(1._dp, fluid%phase(1)%relative_permeability, "Relative permeability")
+       call test%assert(1._dp, fluid%phase(1)%mass_fraction(1), "Mass fraction")
 
-       call assert_equals(pressure, primary2(1), tol, "Primary")
+       call test%assert(pressure, primary2(1), "Primary")
 
     end if
 
