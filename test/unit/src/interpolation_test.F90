1fd07d88997c92b2653c46cb18a060be449307ef
Adrian Croucher <a.croucher@auckland.ac.nz>
2018-11-19 14:20:52 +1300

    Convert interpolation tests to Zofu

--- test/unit/src/interpolation_test.F90
+++ test/unit/src/interpolation_test.F90
@@ -6,7 +6,7 @@ module interpolation_test
 
   use petscsys
   use kinds_module
-  use fruit
+  use zofu
   use interpolation_module
 
   implicit none
@@ -22,8 +22,8 @@ module interpolation_test
        2._dp, 3._dp, 4._dp, &
        3._dp, 4._dp, 5._dp], &
        [3, 4])
-  PetscReal, parameter :: tol = 1.e-9_dp
 
+  public :: setup, teardown, setup_test
   public :: test_interpolation_linear, test_interpolation_single, &
        test_interpolation_step, test_average_linear, test_average_step, &
        test_average_linear_integration, test_interpolation_linear_array, &
@@ -33,10 +33,46 @@ contains
 
 !------------------------------------------------------------------------
 
-  subroutine test_interpolation_linear
+  subroutine setup()
+
+    use profiling_module, only: init_profiling
+
+    ! Locals:
+    PetscErrorCode :: ierr
+
+    call PetscInitialize(PETSC_NULL_CHARACTER, ierr); CHKERRQ(ierr)
+    call init_profiling()
+
+  end subroutine setup
+
+!------------------------------------------------------------------------
+
+  subroutine teardown()
+
+    PetscErrorCode :: ierr
+
+    call PetscFinalize(ierr); CHKERRQ(ierr)
+
+  end subroutine teardown
+
+!------------------------------------------------------------------------
+
+  subroutine setup_test(test)
+
+    class(unit_test_type), intent(in out) :: test
+
+    test%tolerance = 1.e-9
+
+  end subroutine setup_test
+
+!------------------------------------------------------------------------
+
+  subroutine test_interpolation_linear(test)
 
     ! Linear interpolation
 
+    class(unit_test_type), intent(in out) :: test
+    ! Locals:
     type(interpolation_table_type) :: table
     PetscMPIInt :: rank
     PetscInt :: ierr
@@ -47,29 +83,29 @@ contains
 
        call table%init(data5, err = err) ! default linear interpolation
 
-       call assert_equals(0, err, "error")
-       call assert_equals(1, table%dim, "dim")
+       call test%assert(0, err, "error")
+       call test%assert(1, table%dim, "dim")
 
-       call assert_equals(1._dp, table%interpolate(-0.5_dp, 1), tol, "-0.5")
-       call assert_equals(0, table%coord%index, "-0.5 index")
+       call test%assert(1._dp, table%interpolate(-0.5_dp, 1), "-0.5")
+       call test%assert(0, table%coord%index, "-0.5 index")
 
-       call assert_equals(1._dp, table%interpolate(0.0_dp, 1), tol, "0.0")
-       call assert_equals(0, table%coord%index, "0.0 index")
+       call test%assert(1._dp, table%interpolate(0.0_dp, 1), "0.0")
+       call test%assert(0, table%coord%index, "0.0 index")
 
-       call assert_equals(1.4761904761904763_dp, table%interpolate(1.0_dp, 1), tol, "1.0")
-       call assert_equals(1, table%coord%index, "1.0 index")
+       call test%assert(1.4761904761904763_dp, table%interpolate(1.0_dp, 1), "1.0")
+       call test%assert(1, table%coord%index, "1.0 index")
 
-       call assert_equals(0.007692307692307665_dp, table%interpolate(4.5_dp, 1), tol, "4.5")
-       call assert_equals(3, table%coord%index, "4.5 index")
+       call test%assert(0.007692307692307665_dp, table%interpolate(4.5_dp, 1), "4.5")
+       call test%assert(3, table%coord%index, "4.5 index")
 
-       call assert_equals(0.59375_dp, table%interpolate(3.6_dp, 1), tol, "3.6")
-       call assert_equals(2, table%coord%index, "3.6 index")
+       call test%assert(0.59375_dp, table%interpolate(3.6_dp, 1), "3.6")
+       call test%assert(2, table%coord%index, "3.6 index")
 
-       call assert_equals(-1.1_dp, table%interpolate(6.3_dp, 1), tol, "6.3")
-       call assert_equals(4, table%coord%index, "6.3 index")
+       call test%assert(-1.1_dp, table%interpolate(6.3_dp, 1), "6.3")
+       call test%assert(4, table%coord%index, "6.3 index")
 
-       call assert_equals(-0.1_dp, table%interpolate(10.0_dp, 1), tol, "10.0")
-       call assert_equals(5, table%coord%index, "10.0 index")
+       call test%assert(-0.1_dp, table%interpolate(10.0_dp, 1), "10.0")
+       call test%assert(5, table%coord%index, "10.0 index")
 
        call table%destroy()
 
@@ -79,10 +115,12 @@ contains
 
 !------------------------------------------------------------------------
 
-  subroutine test_interpolation_single
+  subroutine test_interpolation_single(test)
 
     ! Single-value data table
 
+    class(unit_test_type), intent(in out) :: test
+    ! Locals:
     type(interpolation_table_type) :: table
     PetscReal, dimension(1,2), parameter :: data1 = reshape(&
          [0._dp, 2._dp], [1,2])
@@ -94,13 +132,13 @@ contains
     if (rank == 0) then
 
        call table%init(data1, err = err)
-       call assert_equals(0, err, "error")
+       call test%assert(0, err, "error")
 
-       call assert_equals(2._dp, table%interpolate(-0.5_dp, 1), tol, "-0.5")
+       call test%assert(2._dp, table%interpolate(-0.5_dp, 1), "-0.5")
 
-       call assert_equals(2._dp, table%interpolate(0.0_dp, 1), tol, "0.0")
+       call test%assert(2._dp, table%interpolate(0.0_dp, 1), "0.0")
 
-       call assert_equals(2._dp, table%interpolate(1.1_dp, 1), tol, "1.1")
+       call test%assert(2._dp, table%interpolate(1.1_dp, 1), "1.1")
 
        call table%destroy()
     end if
@@ -109,10 +147,12 @@ contains
 
 !------------------------------------------------------------------------
 
-  subroutine test_interpolation_step
+  subroutine test_interpolation_step(test)
 
     ! Step interpolation
 
+    class(unit_test_type), intent(in out) :: test
+    ! Locals:
     type(interpolation_table_type) :: table
     PetscMPIInt :: rank
     PetscInt :: ierr
@@ -123,20 +163,20 @@ contains
 
        call table%init(data5, INTERP_STEP, err = err)
 
-       call assert_equals(0, err, "error")
-       call assert_equals(1._dp, table%interpolate(-0.5_dp, 1), tol, "-0.5")
+       call test%assert(0, err, "error")
+       call test%assert(1._dp, table%interpolate(-0.5_dp, 1), "-0.5")
 
-       call assert_equals(1._dp, table%interpolate(0.0_dp, 1), tol, "0.0")
+       call test%assert(1._dp, table%interpolate(0.0_dp, 1), "0.0")
 
-       call assert_equals(1._dp, table%interpolate(1.0_dp, 1), tol, "1.0")
+       call test%assert(1._dp, table%interpolate(1.0_dp, 1), "1.0")
 
-       call assert_equals(0.5_dp, table%interpolate(4.5_dp, 1), tol, "4.5")
+       call test%assert(0.5_dp, table%interpolate(4.5_dp, 1), "4.5")
 
-       call assert_equals(2.0_dp, table%interpolate(3.6_dp, 1), tol, "3.6")
+       call test%assert(2.0_dp, table%interpolate(3.6_dp, 1), "3.6")
 
-       call assert_equals(-1.1_dp, table%interpolate(6.3_dp, 1), tol, "6.3")
+       call test%assert(-1.1_dp, table%interpolate(6.3_dp, 1), "6.3")
 
-       call assert_equals(-0.1_dp, table%interpolate(10.0_dp, 1), tol, "10.0")
+       call test%assert(-0.1_dp, table%interpolate(10.0_dp, 1), "10.0")
 
        call table%destroy()
 
@@ -146,10 +186,12 @@ contains
 
 !------------------------------------------------------------------------
 
-  subroutine test_average_linear
+  subroutine test_average_linear(test)
 
     ! Linear interpolation average
 
+    class(unit_test_type), intent(in out) :: test
+    ! Locals:
     type(interpolation_table_type) :: table
     PetscMPIInt :: rank
     PetscInt :: ierr
@@ -161,21 +203,21 @@ contains
 
        call table%init(data5, err = err)
 
-       call assert_equals(0, err, "error")
-       call assert_equals(1._dp, table%average([-0.5_dp, -0.1_dp], 1), tol, "[-0.5, -0.1]")
+       call test%assert(0, err, "error")
+       call test%assert(1._dp, table%average([-0.5_dp, -0.1_dp], 1), "[-0.5, -0.1]")
 
-       call assert_equals(1.0238095238095237_dp, table%average([-0.5_dp, 0.1_dp], 1), &
-            tol, "[-0.5, 0.1]")
+       call test%assert(1.0238095238095237_dp, table%average([-0.5_dp, 0.1_dp], 1), &
+            "[-0.5, 0.1]")
 
-       call assert_equals(1.5_dp, table%average([0.1_dp, 2._dp], 1), tol, "[0.1, 2.]")
+       call test%assert(1.5_dp, table%average([0.1_dp, 2._dp], 1), "[0.1, 2.]")
 
-       call assert_equals(1.1019345238095237_dp, table%average([0.1_dp, 3._dp], 1), tol, "[0.1, 3.]")
+       call test%assert(1.1019345238095237_dp, table%average([0.1_dp, 3._dp], 1), "[0.1, 3.]")
 
-       call assert_equals(0.11586538461538454_dp, table%average([3.1_dp, 7._dp], 1), tol, "[3.1, 7.]")
+       call test%assert(0.11586538461538454_dp, table%average([3.1_dp, 7._dp], 1), "[3.1, 7.]")
 
-       call assert_equals(-0.27307692307692316_dp, table%average([8._dp, 12._dp], 1), tol, "[8., 12.]")
+       call test%assert(-0.27307692307692316_dp, table%average([8._dp, 12._dp], 1), "[8., 12.]")
 
-       call assert_equals(1.4761904761904763_dp, table%average([1._dp, 1._dp], 1), tol, "[1., 1.]")
+       call test%assert(1.4761904761904763_dp, table%average([1._dp, 1._dp], 1), "[1., 1.]")
 
        call table%destroy()
 
@@ -185,10 +227,12 @@ contains
 
 !------------------------------------------------------------------------
 
-  subroutine test_average_step
+  subroutine test_average_step(test)
 
     ! Step interpolation average
 
+    class(unit_test_type), intent(in out) :: test
+    ! Locals:
     type(interpolation_table_type) :: table
     PetscMPIInt :: rank
     PetscInt :: ierr
@@ -199,21 +243,21 @@ contains
     if (rank == 0) then
 
        call table%init(data5, INTERP_STEP, err = err)
-       call assert_equals(0, err, "error")
+       call test%assert(0, err, "error")
 
-       call assert_equals(1._dp, table%average([-0.5_dp, -0.1_dp], 1), tol, "[-0.5, -0.1]")
+       call test%assert(1._dp, table%average([-0.5_dp, -0.1_dp], 1), "[-0.5, -0.1]")
 
-       call assert_equals(1._dp, table%average([-0.5_dp, 0.1_dp], 1), tol, "[-0.5, 0.1]")
+       call test%assert(1._dp, table%average([-0.5_dp, 0.1_dp], 1), "[-0.5, 0.1]")
 
-       call assert_equals(1._dp, table%average([0.1_dp, 2._dp], 1), tol, "[0.1, 2.]")
+       call test%assert(1._dp, table%average([0.1_dp, 2._dp], 1), "[0.1, 2.]")
 
-       call assert_equals(1.5_dp, table%average([0.1_dp, 3._dp], 1), tol, "[0.1, 3.]")
+       call test%assert(1.5_dp, table%average([0.1_dp, 3._dp], 1), "[0.1, 3.]")
 
-       call assert_equals(0.45_dp, table%average([3.1_dp, 7._dp], 1), tol, "[3.1, 7.]")
+       call test%assert(0.45_dp, table%average([3.1_dp, 7._dp], 1), "[3.1, 7.]")
 
-       call assert_equals(-0.6_dp, table%average([8._dp, 12._dp], 1), tol, "[8., 12.]")
+       call test%assert(-0.6_dp, table%average([8._dp, 12._dp], 1), "[8., 12.]")
 
-       call assert_equals(1._dp, table%average([1._dp, 1._dp], 1), tol, "[1., 1.]")
+       call test%assert(1._dp, table%average([1._dp, 1._dp], 1), "[1., 1.]")
 
        call table%destroy()
 
@@ -223,10 +267,12 @@ contains
 
 !------------------------------------------------------------------------
 
-  subroutine test_average_linear_integration
+  subroutine test_average_linear_integration(test)
 
     ! Linear interpolation integration averaging
 
+    class(unit_test_type), intent(in out) :: test
+    ! Locals:
     type(interpolation_table_type) :: table
     PetscMPIInt :: rank
     PetscInt :: ierr
@@ -236,24 +282,24 @@ contains
     if (rank == 0) then
 
        call table%init(data5, INTERP_LINEAR, INTERP_AVERAGING_INTEGRATE, err = err)
-       call assert_equals(0, err, "error")
+       call test%assert(0, err, "error")
 
-       call assert_equals(1._dp, table%average([-0.5_dp, -0.1_dp], 1), tol, "[-0.5, -0.1]")
+       call test%assert(1._dp, table%average([-0.5_dp, -0.1_dp], 1), "[-0.5, -0.1]")
 
-       call assert_equals(1.003968253968254_dp, table%average([-0.5_dp, 0.1_dp], &
-            1), tol, "[-0.5, 0.1]")
+       call test%assert(1.003968253968254_dp, table%average([-0.5_dp, 0.1_dp], &
+            1), "[-0.5, 0.1]")
 
-       call assert_equals(1.5_dp, table%average([0.1_dp, 2._dp], 1), tol, "[0.1, 2.]")
+       call test%assert(1.5_dp, table%average([0.1_dp, 2._dp], 1), "[0.1, 2.]")
 
-       call assert_equals(1.5406660509031198_dp, table%average([0.1_dp, 3._dp], 1), tol, "[0.1, 3.]")
+       call test%assert(1.5406660509031198_dp, table%average([0.1_dp, 3._dp], 1), "[0.1, 3.]")
 
-       call assert_equals(-0.2530818540433925_dp, table%average([3.1_dp, 7._dp], 1), tol, "[3.1, 7.]")
+       call test%assert(-0.2530818540433925_dp, table%average([3.1_dp, 7._dp], 1), "[3.1, 7.]")
 
-       call assert_equals(-0.1389423076923077_dp, table%average([8._dp, 12._dp], 1), tol, "[8., 12.]")
+       call test%assert(-0.1389423076923077_dp, table%average([8._dp, 12._dp], 1), "[8., 12.]")
 
-       call assert_equals(-0.1_dp, table%average([9._dp, 12._dp], 1), tol, "[9., 12.]")
+       call test%assert(-0.1_dp, table%average([9._dp, 12._dp], 1), "[9., 12.]")
 
-       call assert_equals(1.4761904761904763_dp, table%average([1._dp, 1._dp], 1), tol, "[1., 1.]")
+       call test%assert(1.4761904761904763_dp, table%average([1._dp, 1._dp], 1), "[1., 1.]")
 
        call table%destroy()
 
@@ -263,9 +309,11 @@ contains
 
 !------------------------------------------------------------------------
 
-  subroutine test_interpolation_linear_array
+  subroutine test_interpolation_linear_array(test)
     ! Array interpolation
 
+    class(unit_test_type), intent(in out) :: test
+    ! Locals:
     type(interpolation_table_type) :: table
     PetscMPIInt :: rank
     PetscInt :: ierr
@@ -275,17 +323,17 @@ contains
     if (rank == 0) then
 
        call table%init(data3, err = err)
-       call assert_equals(0, err, "error")
-       call assert_equals(3, table%dim, "dim")
+       call test%assert(0, err, "error")
+       call test%assert(3, table%dim, "dim")
 
-       call assert_equals([1._dp, 2._dp, 3._dp], table%interpolate(0._dp), 3, tol, "0")
-       call assert_equals(0, table%coord%index, "0 index")
+       call test%assert([1._dp, 2._dp, 3._dp], table%interpolate(0._dp), "0")
+       call test%assert(0, table%coord%index, "0 index")
 
-       call assert_equals([1.5_dp, 2.5_dp, 3.5_dp], table%interpolate(0.5_dp), 3, tol, "0.5")
-       call assert_equals(1, table%coord%index, "0.5 index")
+       call test%assert([1.5_dp, 2.5_dp, 3.5_dp], table%interpolate(0.5_dp), "0.5")
+       call test%assert(1, table%coord%index, "0.5 index")
 
-       call assert_equals([2.5_dp, 3.5_dp, 4.5_dp], table%interpolate(1.5_dp), 3, tol, "1.5")
-       call assert_equals(2, table%coord%index, "1.5 index")
+       call test%assert([2.5_dp, 3.5_dp, 4.5_dp], table%interpolate(1.5_dp), "1.5")
+       call test%assert(2, table%coord%index, "1.5 index")
 
        call table%destroy()
 
@@ -295,9 +343,11 @@ contains
 
 !------------------------------------------------------------------------
 
-  subroutine test_find
+  subroutine test_find(test)
     ! Find_component_at_index()
 
+    class(unit_test_type), intent(in out) :: test
+    ! Locals:
     type(interpolation_table_type) :: table
     PetscMPIInt :: rank
     PetscInt :: ierr
@@ -308,18 +358,18 @@ contains
     if (rank == 0) then
 
        call table%init(data3, err = err)
-       call assert_equals(0, err, "error")
+       call test%assert(0, err, "error")
 
        call table%find_component_at_index(1.5_dp, 1, x, err)
-       call assert_equals(0.5_dp, x, tol, "component 1 = 1.5")
-       call assert_equals(0, err, "component 1 = 1.5 error")
+       call test%assert(0.5_dp, x, "component 1 = 1.5")
+       call test%assert(0, err, "component 1 = 1.5 error")
 
        call table%find_component_at_index(3.75_dp, 3, x, err)
-       call assert_equals(0.75_dp, x, tol, "component 3 = 3.75")
-       call assert_equals(0, err, "component 3 = 3.75 error")
+       call test%assert(0.75_dp, x, "component 3 = 3.75")
+       call test%assert(0, err, "component 3 = 3.75 error")
 
        call table%find_component_at_index(0._dp, 1, x, err)
-       call assert_equals(0, err, "component 1 = 0 error")
+       call test%assert(0, err, "component 1 = 0 error")
        
        call table%destroy()
 
@@ -330,9 +380,11 @@ contains
 
 !------------------------------------------------------------------------
 
-  subroutine test_unsorted
+  subroutine test_unsorted(test)
     ! Unsorted table
 
+    class(unit_test_type), intent(in out) :: test
+    ! Locals:
     type(interpolation_table_type) :: table
     PetscMPIInt :: rank
     PetscInt :: ierr
@@ -346,7 +398,7 @@ contains
     if (rank == 0) then
 
        call table%init(data, err = err)
-       call assert_equals(1, err, "error")
+       call test%assert(1, err, "error")
        call table%destroy()
 
     end if
@@ -355,9 +407,11 @@ contains
 
 !------------------------------------------------------------------------
 
-  subroutine test_duplicate
+  subroutine test_duplicate(test)
     ! Duplicate coordinates
 
+    class(unit_test_type), intent(in out) :: test
+    ! Locals:
     type(interpolation_table_type) :: table
     PetscMPIInt :: rank
     PetscInt :: ierr
@@ -371,11 +425,11 @@ contains
     if (rank == 0) then
 
        call table%init(data, err = err)
-       call assert_equals(0, err, "error")
-       call assert_equals(0.3_dp, table%interpolate(1.5_dp, 1), tol, "1.5")
-       call assert_equals(3, table%coord%index, "1.5 index")
-       call assert_equals(0.5_dp, table%interpolate(1.0_dp, 1), tol, "1.0")
-       call assert_equals(3, table%coord%index, "1.0 index")
+       call test%assert(0, err, "error")
+       call test%assert(0.3_dp, table%interpolate(1.5_dp, 1), "1.5")
+       call test%assert(3, table%coord%index, "1.5 index")
+       call test%assert(0.5_dp, table%interpolate(1.0_dp, 1), "1.0")
+       call test%assert(3, table%coord%index, "1.0 index")
        call table%destroy()
 
     end if
@@ -384,4 +438,4 @@ contains
 
 !------------------------------------------------------------------------
 
-     end module interpolation_test
+end module interpolation_test
