f9c5dfed7a3ff8da03ebc12bfa77a484789a2565
Adrian Croucher <a.croucher@auckland.ac.nz>
2018-11-19 16:14:10 +1300

    Convert NCG air thermodynamics unit tests to Zofu

--- test/unit/src/ncg_air_thermodynamics_test.F90
+++ test/unit/src/ncg_air_thermodynamics_test.F90
@@ -7,11 +7,12 @@ module ncg_air_thermodynamics_test
   use petscsys
   use kinds_module
   use ncg_air_thermodynamics_module
-  use fruit
+  use zofu
 
   implicit none
   private
 
+  public :: setup, teardown
   public :: test_ncg_air_enthalpy, test_ncg_air_henry, &
        test_ncg_air_energy_solution, test_ncg_air_mixture_viscosity
 
@@ -19,9 +20,35 @@ contains
 
 !------------------------------------------------------------------------
 
-  subroutine test_ncg_air_enthalpy
+  subroutine setup()
+
+    use profiling_module, only: init_profiling
+
+    ! Locals:
+    PetscErrorCode :: ierr
+
+    call PetscInitialize(PETSC_NULL_CHARACTER, ierr); CHKERRQ(ierr)
+    call init_profiling()
+
+  end subroutine setup
+
+!------------------------------------------------------------------------
+
+  subroutine teardown()
+
+    PetscErrorCode :: ierr
+
+    call PetscFinalize(ierr); CHKERRQ(ierr)
+
+  end subroutine teardown
+
+!------------------------------------------------------------------------
+
+  subroutine test_ncg_air_enthalpy(test)
     ! Air enthalpy
 
+    class(unit_test_type), intent(in out) :: test
+    ! Locals:
     type(ncg_air_thermodynamics_type) :: gas
     PetscReal :: temperature, expected, props(2)
     PetscErrorCode :: err
@@ -29,7 +56,6 @@ contains
     PetscMPIInt :: rank
     PetscInt :: ierr
     PetscReal, parameter :: pa = 1.e5_dp
-    PetscReal, parameter :: tol = 1.e-6_dp
 
     call gas%init()
 
@@ -40,32 +66,32 @@ contains
        temperature = 20._dp
        expected = 19766.68740112_dp
        call gas%properties(pa, temperature, props, err)
-       call assert_equals(0, err, trim(s) // " 20 deg C error")
-       call assert_equals(expected, props(2), tol, trim(s) // " 20 deg C")
+       call test%assert(0, err, trim(s) // " 20 deg C error")
+       call test%assert(expected, props(2), trim(s) // " 20 deg C")
 
        temperature = 100._dp
        expected = 99758.4176379_dp
        call gas%properties(pa, temperature, props, err)
-       call assert_equals(0, err, trim(s) // " 20 deg C error")
-       call assert_equals(expected, props(2), tol, trim(s) // " 20 deg C")
+       call test%assert(0, err, trim(s) // " 20 deg C error")
+       call test%assert(expected, props(2), trim(s) // " 20 deg C")
 
        temperature = 240._dp
        expected = 243111.52506676_dp
        call gas%properties(pa, temperature, props, err)
-       call assert_equals(0, err, trim(s) // " 20 deg C error")
-       call assert_equals(expected, props(2), tol, trim(s) // " 20 deg C")
+       call test%assert(0, err, trim(s) // " 20 deg C error")
+       call test%assert(expected, props(2), trim(s) // " 20 deg C")
 
        temperature = 300._dp
        expected = 305841.67918959_dp
        call gas%properties(pa, temperature, props, err)
-       call assert_equals(0, err, trim(s) // " 20 deg C error")
-       call assert_equals(expected, props(2), tol, trim(s) // " 20 deg C")
+       call test%assert(0, err, trim(s) // " 20 deg C error")
+       call test%assert(expected, props(2), trim(s) // " 20 deg C")
 
        temperature = 350._dp
        expected = 358701.73310281_dp
        call gas%properties(pa, temperature, props, err)
-       call assert_equals(0, err, trim(s) // " 20 deg C error")
-       call assert_equals(expected, props(2), tol, trim(s) // " 20 deg C")
+       call test%assert(0, err, trim(s) // " 20 deg C error")
+       call test%assert(expected, props(2), trim(s) // " 20 deg C")
 
     end if
 
@@ -73,16 +99,17 @@ contains
 
 !------------------------------------------------------------------------
 
-  subroutine test_ncg_air_henry
-    ! Air Henry's constant
+  subroutine test_ncg_air_henry(test)
+    ! Air Henry constant
 
+    class(unit_test_type), intent(in out) :: test
+    ! Locals:
     type(ncg_air_thermodynamics_type) :: gas
     PetscReal :: temperature, expected, hc
     PetscErrorCode :: err
-    character(33) :: s = "Air Henry's constant, temperature"
+    character(33) :: s = "Air Henry constant, temperature"
     PetscMPIInt :: rank
     PetscInt :: ierr
-    PetscReal, parameter :: tol = 1.e-15_dp
 
     call gas%init()
 
@@ -93,32 +120,32 @@ contains
        temperature = 20._dp
        expected = 1.37591939e-10_dp
        call gas%henrys_constant(temperature, hc, err)
-       call assert_equals(0, err, trim(s) // " 20 deg C error")
-       call assert_equals(expected, hc, tol, trim(s) // " 20 deg C")
+       call test%assert(0, err, trim(s) // " 20 deg C error")
+       call test%assert(expected, hc, trim(s) // " 20 deg C")
 
        temperature = 100._dp
        expected = 8.89730840e-11_dp
        call gas%henrys_constant(temperature, hc, err)
-       call assert_equals(0, err, trim(s) // " 100 deg C error")
-       call assert_equals(expected, hc, tol, trim(s) // " 100 deg C")
+       call test%assert(0, err, trim(s) // " 100 deg C error")
+       call test%assert(expected, hc, trim(s) // " 100 deg C")
 
        temperature = 240._dp
        expected = 2.52702897e-10_dp
        call gas%henrys_constant(temperature, hc, err)
-       call assert_equals(0, err, trim(s) // " 240 deg C error")
-       call assert_equals(expected, hc, tol, trim(s) // " 240 deg C")
+       call test%assert(0, err, trim(s) // " 240 deg C error")
+       call test%assert(expected, hc, trim(s) // " 240 deg C")
 
        temperature = 300._dp
        expected = 5.25847886e-10_dp
        call gas%henrys_constant(temperature, hc, err)
-       call assert_equals(0, err, trim(s) // " 300 deg C error")
-       call assert_equals(expected, hc, tol, trim(s) // " 300 deg C")
+       call test%assert(0, err, trim(s) // " 300 deg C error")
+       call test%assert(expected, hc, trim(s) // " 300 deg C")
 
        temperature = 350._dp
        expected = 1.63742619e-09_dp
        call gas%henrys_constant(temperature, hc, err)
-       call assert_equals(0, err, trim(s) // " 300 deg C error")
-       call assert_equals(expected, hc, tol, trim(s) // " 300 deg C")
+       call test%assert(0, err, trim(s) // " 300 deg C error")
+       call test%assert(expected, hc, trim(s) // " 300 deg C")
 
     end if
 
@@ -126,16 +153,17 @@ contains
 
 !------------------------------------------------------------------------
 
-  subroutine test_ncg_air_energy_solution
+  subroutine test_ncg_air_energy_solution(test)
     ! Air energy of solution
 
+    class(unit_test_type), intent(in out) :: test
+    ! Locals:
     type(ncg_air_thermodynamics_type) :: gas
     PetscReal :: temperature, expected, hs, hc
     PetscErrorCode :: err
     character(35) :: s = "Air energy of solution, temperature"
     PetscMPIInt :: rank
     PetscInt :: ierr
-    PetscReal, parameter :: tol = 1.e-4_dp
 
     call gas%init()
 
@@ -147,36 +175,36 @@ contains
        expected = -400992.95145511_dp
        call gas%henrys_constant(temperature, hc, err)
        call gas%energy_solution(temperature, hc, hs, err)
-       call assert_equals(0, err, trim(s) // " 20 deg C error")
-       call assert_equals(expected, hs, tol, trim(s) // " 20 deg C")
+       call test%assert(0, err, trim(s) // " 20 deg C error")
+       call test%assert(expected, hs, trim(s) // " 20 deg C")
 
        temperature = 100._dp
        expected = 58149.1888649_dp
        call gas%henrys_constant(temperature, hc, err)
        call gas%energy_solution(temperature, hc, hs, err)
-       call assert_equals(0, err, trim(s) // " 100 deg C error")
-       call assert_equals(expected, hs, tol, trim(s) // " 100 deg C")
+       call test%assert(0, err, trim(s) // " 100 deg C error")
+       call test%assert(expected, hs, trim(s) // " 100 deg C")
 
        temperature = 240._dp
        expected = 845139.95259237_dp
        call gas%henrys_constant(temperature, hc, err)
        call gas%energy_solution(temperature, hc, hs, err)
-       call assert_equals(0, err, trim(s) // " 240 deg C error")
-       call assert_equals(expected, hs, tol, trim(s) // " 240 deg C")
+       call test%assert(0, err, trim(s) // " 240 deg C error")
+       call test%assert(expected, hs, trim(s) // " 240 deg C")
 
        temperature = 300._dp
        expected = 1358812.70708159_dp
        call gas%henrys_constant(temperature, hc, err)
        call gas%energy_solution(temperature, hc, hs, err)
-       call assert_equals(0, err, trim(s) // " 300 deg C error")
-       call assert_equals(expected, hs, tol, trim(s) // " 300 deg C")
+       call test%assert(0, err, trim(s) // " 300 deg C error")
+       call test%assert(expected, hs, trim(s) // " 300 deg C")
 
        temperature = 350._dp
        expected = 4390404.09402286_dp
        call gas%henrys_constant(temperature, hc, err)
        call gas%energy_solution(temperature, hc, hs, err)
-       call assert_equals(0, err, trim(s) // " 350 deg C error")
-       call assert_equals(expected, hs, tol, trim(s) // " 350 deg C")
+       call test%assert(0, err, trim(s) // " 350 deg C error")
+       call test%assert(expected, hs, trim(s) // " 350 deg C")
 
     end if
 
@@ -184,11 +212,13 @@ contains
 
 !------------------------------------------------------------------------
 
-    subroutine test_ncg_air_mixture_viscosity
+    subroutine test_ncg_air_mixture_viscosity(test)
     ! Water-air mixture viscosity
 
     use IFC67_module
 
+    class(unit_test_type), intent(in out) :: test
+    ! Locals:
     type(ncg_air_thermodynamics_type) :: gas
     PetscMPIInt :: rank
     PetscInt :: ierr, err
@@ -198,7 +228,6 @@ contains
     type(IFC67_type) :: thermo
     PetscReal, parameter :: Pair = 0.e5_dp ! not used
     PetscInt, parameter :: phase = 2 ! vapour phase
-    PetscReal, parameter :: tol = 1.e-9_dp
     PetscInt, parameter :: num_cases = 3
     PetscReal, parameter :: t(num_cases) = [240._dp, 120._dp, 20._dp]
     PetscReal, parameter :: xg(num_cases) = [0.1_dp, 0.5_dp, 0.8_dp]
@@ -206,6 +235,7 @@ contains
          0.171595480e-4_dp, 0.128139659e-4_dp, 8.73278989112e-6_dp]
     PetscReal, parameter :: expected_visc(num_cases) = [ &
          1.81800535828e-05_dp, 0.178797007e-4_dp, 1.67537163543e-5_dp]
+    PetscReal, parameter :: tol = 1.e-5_dp
 
     call thermo%init()
     call gas%init()
@@ -218,8 +248,8 @@ contains
           call gas%mixture_viscosity(water_viscosity(i), t(i), Pair, &
                xg(i), phase, visc, err)
           write(s, '(a, i2)') 'case ', i
-          call assert_equals(0, err, trim(s) // ' error')
-          call assert_equals(expected_visc(i), visc, tol, s)
+          call test%assert(0, err, trim(s) // ' error')
+          call test%assert(expected_visc(i), visc, s, tol)
        end do
 
     end if
