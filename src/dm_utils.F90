0fb2da8c9155aa6cda2c434daaca77fc7a417f95
Adrian Croucher <a.croucher@auckland.ac.nz>
2019-07-15 17:43:37 +1200

    Bugfix in dm_distribute_global_vec(): use correct DM for local -> global

--- src/dm_utils.F90
+++ src/dm_utils.F90
@@ -1340,9 +1340,9 @@ contains
     call PetscObjectGetName(v, name, ierr); CHKERRQ(ierr)
     call PetscObjectSetName(global_v, name, ierr); CHKERRQ(ierr)
 
-    call DMLocalToGlobalBegin(dm, local_v, INSERT_VALUES, global_v, ierr)
+    call DMLocalToGlobalBegin(dist_v_dm, local_v, INSERT_VALUES, global_v, ierr)
     CHKERRQ(ierr)
-    call DMLocalToGlobalEnd(dm, local_v, INSERT_VALUES, global_v, ierr)
+    call DMLocalToGlobalEnd(dist_v_dm, local_v, INSERT_VALUES, global_v, ierr)
     CHKERRQ(ierr)
 
     call VecDestroy(local_v, ierr); CHKERRQ(ierr)
