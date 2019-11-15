0cba7255215bbb061ab2680e0201e7779f321e0e
Tim Harton <tim.harton@gmail.com>
2019-11-15 17:43:40 +1300

    updated moved deploy directory to install directory and setup docker yml script

--- deploy/docs/v0.3/waiwera.ps1
+++
@@ -1,3 +0,0 @@
-$p = "/",(($pwd -replace "\\","/") -replace ":","").ToLower().Trim("/") -join ""
-# echo $p $args[0] $args[1]
-docker run -v ${p}:/data -w /data waiwera-phusion-debian mpiexec -np $args[1] /home/mpirun/waiwera/dist/waiwera $args[0]
--- deploy/docs/v0.3/waiwera.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo running $1 in $(pwd)
-# docker run -v $(pwd):/data -w /data alpine ls
-docker run -v $(pwd):/data -w /data waiwera-phusion-debian mpiexec -np $2 /home/mpirun/waiwera/dist/waiwera $1
