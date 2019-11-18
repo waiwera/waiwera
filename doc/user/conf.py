1e2822bf849c29474e3e9e3a99a03fcba579d4c9
Adrian Croucher <a.croucher@auckland.ac.nz>
2019-11-18 13:31:03 +1300

    Add logos to html and PDF versions of user guide

--- doc/user/conf.py
+++ doc/user/conf.py
@@ -146,7 +146,7 @@ html_theme = 'pyramid'
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
 #
-# html_logo = None
+html_logo = 'waiwera_logo_text.png'
 
 # The name of an image file (relative to this directory) to use as a favicon of
 # the docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
@@ -273,7 +273,7 @@ latex_documents = [
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
 #
-# latex_logo = None
+latex_logo = 'waiwera_logo.pdf'
 
 # For "manual" documents, if this is true, then toplevel headings are parts,
 # not chapters.
