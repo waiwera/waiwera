ee54e4763ec273df97cc7a8c8093f99cf6ec53eb
Angus Yeh <a.yeh@auckland.ac.nz>
2020-01-28 10:23:04 +1300

    Fix issue of pywaiwera.__version__ when module loaded from PYTHONPATH (CI failed as package is not built/installed before unit test).

--- utils/pywaiwera/pywaiwera/common.py
+++ utils/pywaiwera/pywaiwera/common.py
@@ -1,9 +1,33 @@
 # this requires setuptools
 import pkg_resources
+import os
+import re
 
 def get_pkg_version():
-    dist = pkg_resources.get_distribution("pywaiwera")
-    return dist.version
+    """ use python's package info, if loaded as a packaged module """
+    try:
+        dist = pkg_resources.get_distribution("pywaiwera")
+        return dist.version
+    except pkg_resources.DistributionNotFound:
+        return None
+
+def get_f90_version():
+    """ use official waiwera version number from fortran source, if loaded from
+    PYTHONPATH """
+    here = os.path.dirname(os.path.abspath(__file__))
+    vf90 = os.path.abspath(os.path.join(here, '..', '..', '..', 'src', 'version.F90'))
+    if not os.path.isfile(vf90):
+        raise Exception('Unable to find waiwera/src/version.F90.')
+    with open('../../src/version.F90', 'r') as fv:
+        matches = re.findall('waiwera_version += +"(.+?)"', fv.read())
+        if matches:
+            return matches[0]
+        else:
+            raise Exception('Unable to parse version string from version.F90.')
 
 __version__ = get_pkg_version()
+if __version__ is None:
+    __version__ = get_f90_version()
+
+
 
