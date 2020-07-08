b28d54c4e523c4bc00c3742ec210bc870be9bc0e
Adrian Croucher <a.croucher@auckland.ac.nz>
2020-07-08 17:38:07 +1200

    Return tracer array in setup_tracer()

--- src/tracer.F90
+++ src/tracer.F90
@@ -42,12 +42,13 @@ contains
 !------------------------------------------------------------------------
 
   subroutine setup_tracer(json, dm, auxiliary, aux_solution, range_start, &
-       num_tracers)
+       tracers)
 
     !! Sets up tracers from JSON input.
 
     use fson
     use fson_mpi_module
+    use kinds_module
     use dm_utils_module, only: dm_set_data_layout, global_vec_range_start
 
     type(fson_value), pointer, intent(in) :: json
@@ -55,34 +56,48 @@ contains
     PetscBool, intent(out) :: auxiliary
     Vec, intent(in out) :: aux_solution
     PetscInt, intent(out) :: range_start
-    PetscInt, intent(out) :: num_tracers
+    type(tracer_type), allocatable, intent(out) :: tracers(:)
     ! Locals:
     DM :: dm_aux
-    PetscInt :: dim
+    PetscInt :: dim, i, num_tracers
     PetscInt, allocatable :: tracer_num_components(:), tracer_dim(:)
     character(max_tracer_name_length), allocatable :: tracer_names(:)
+    PetscReal, parameter :: default_decay_rate = 0._dp
     PetscErrorCode :: ierr
 
     if (fson_has_mpi(json, "tracer")) then
+
        auxiliary = PETSC_TRUE
        call DMClone(dm, dm_aux, ierr); CHKERRQ(ierr)
        call DMGetDimension(dm, dim, ierr); CHKERRQ(ierr)
+
        ! TODO get tracer parameters
        num_tracers = 1
+       allocate(tracers(num_tracers))
+
        allocate(tracer_num_components(num_tracers))
        tracer_num_components = 1
        allocate(tracer_dim(num_tracers))
        tracer_dim = dim
-       tracer_names = ["tracer"]
+       allocate(tracer_names(num_tracers))
+
+       tracers(1) = tracer_type("tracer", default_decay_rate)
+       do i = 1, num_tracers
+          tracer_names(i) = tracers(i)%name
+       end do
+
        call dm_set_data_layout(dm_aux, tracer_num_components, &
          tracer_dim, tracer_names)
        call DMCreateGlobalVector(dm_aux, aux_solution, ierr); CHKERRQ(ierr)
        call PetscObjectSetName(aux_solution, "tracer", ierr); CHKERRQ(ierr)
        call global_vec_range_start(aux_solution, range_start)
        call DMDestroy(dm_aux, ierr); CHKERRQ(ierr)
+
        deallocate(tracer_names)
     else
        auxiliary = PETSC_FALSE
+       num_tracers = 0
+       allocate(tracers(num_tracers))
     end if
 
   end subroutine setup_tracer
