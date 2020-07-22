6ee7b7885d1a17590e278b75afb2eb2e894dfffc
Adrian Croucher <a.croucher@auckland.ac.nz>
2020-07-22 14:47:55 +1200

    Rename setup_tracer() to setup_tracers() and factor out create_tracer_vector()

--- src/tracer.F90
+++ src/tracer.F90
@@ -35,72 +35,82 @@ module tracer_module
      PetscReal, public :: decay !! Decay rate (1/s)
   end type tracer_type
 
-  public :: setup_tracer
+  public :: setup_tracers, create_tracer_vector
 
 contains
 
 !------------------------------------------------------------------------
 
-  subroutine setup_tracer(json, dm, auxiliary, aux_solution, range_start, &
-       tracers)
+  subroutine setup_tracers(json, tracers)
 
     !! Sets up tracers from JSON input.
 
     use fson
     use fson_mpi_module
     use kinds_module
-    use dm_utils_module, only: dm_set_data_layout, global_vec_range_start
 
     type(fson_value), pointer, intent(in) :: json
-    DM, intent(in out) :: dm
-    PetscBool, intent(out) :: auxiliary
-    Vec, intent(in out) :: aux_solution
-    PetscInt, intent(out) :: range_start
     type(tracer_type), allocatable, intent(out) :: tracers(:)
     ! Locals:
-    DM :: dm_aux
-    PetscInt :: dim, i, num_tracers
-    PetscInt, allocatable :: tracer_num_components(:), tracer_dim(:)
-    character(max_tracer_name_length), allocatable :: tracer_names(:)
+    PetscInt :: num_tracers
     PetscReal, parameter :: default_decay_rate = 0._dp
-    PetscErrorCode :: ierr
 
     if (fson_has_mpi(json, "tracer")) then
-
-       auxiliary = PETSC_TRUE
-       call DMClone(dm, dm_aux, ierr); CHKERRQ(ierr)
-       call DMGetDimension(dm, dim, ierr); CHKERRQ(ierr)
 
        ! TODO get tracer parameters
        num_tracers = 1
        allocate(tracers(num_tracers))
-
-       allocate(tracer_num_components(num_tracers))
-       tracer_num_components = 1
-       allocate(tracer_dim(num_tracers))
-       tracer_dim = dim
-       allocate(tracer_names(num_tracers))
-
        tracers(1) = tracer_type("tracer", default_decay_rate)
-       do i = 1, num_tracers
-          tracer_names(i) = tracers(i)%name
-       end do
 
-       call dm_set_data_layout(dm_aux, tracer_num_components, &
-         tracer_dim, tracer_names)
-       call DMCreateGlobalVector(dm_aux, aux_solution, ierr); CHKERRQ(ierr)
-       call PetscObjectSetName(aux_solution, "tracer", ierr); CHKERRQ(ierr)
-       call global_vec_range_start(aux_solution, range_start)
-       call DMDestroy(dm_aux, ierr); CHKERRQ(ierr)
-
-       deallocate(tracer_names)
     else
-       auxiliary = PETSC_FALSE
        num_tracers = 0
        allocate(tracers(num_tracers))
     end if
 
-  end subroutine setup_tracer
+  end subroutine setup_tracers
+
+!------------------------------------------------------------------------
+
+  subroutine create_tracer_vector(dm, tracers, tracer_vector, range_start)
+    !! Create tracer solution vector.
+
+    use dm_utils_module, only: dm_set_data_layout, global_vec_range_start
+
+    DM, intent(in out) :: dm
+    type(tracer_type), intent(in) :: tracers(:)
+    Vec, intent(in out) :: tracer_vector
+    PetscInt, intent(out) :: range_start
+    ! Locals:
+    DM :: dm_tracer
+    PetscInt :: dim
+    PetscInt, allocatable :: tracer_num_components(:), tracer_dim(:)
+    character(max_tracer_name_length), allocatable :: tracer_names(:)
+    PetscErrorCode :: ierr
+
+    call DMClone(dm, dm_tracer, ierr); CHKERRQ(ierr)
+    call DMGetDimension(dm, dim, ierr); CHKERRQ(ierr)
+
+    associate(num_tracers => size(tracers))
+
+      allocate(tracer_num_components(num_tracers))
+      tracer_num_components = 1
+      allocate(tracer_dim(num_tracers))
+      tracer_dim = dim
+      allocate(tracer_names(num_tracers))
+      tracer_names = tracers%name
+
+      call dm_set_data_layout(dm_tracer, tracer_num_components, &
+           tracer_dim, tracer_names)
+      call DMCreateGlobalVector(dm_tracer, tracer_vector, ierr); CHKERRQ(ierr)
+      call PetscObjectSetName(tracer_vector, "tracer", ierr); CHKERRQ(ierr)
+      call global_vec_range_start(tracer_vector, range_start)
+
+      call DMDestroy(dm_tracer, ierr); CHKERRQ(ierr)
+      deallocate(tracer_num_components, tracer_dim, tracer_names)
+
+    end associate
+
+  end subroutine create_tracer_vector
 
 !------------------------------------------------------------------------
 
