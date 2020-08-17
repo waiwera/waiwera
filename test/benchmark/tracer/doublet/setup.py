f36bc50c014414dd348bcc517da1a81d6e6cf3a8
Adrian Croucher <a.croucher@auckland.ac.nz>
2020-08-18 11:49:07 +1200

    Add 1-D single-phase tracer doublet benchmark test

---
+++ test/benchmark/tracer/doublet/setup.py
@@ -0,0 +1,170 @@
+# 1-D tracer doublet problem
+from __future__ import print_function
+from t2data import *
+from t2incons import *
+from t2thermo import cowat
+import json
+json.encoder.FLOAT_REPR = lambda o: format(o, '0.12g')
+import os
+import subprocess
+
+AUTOUGH2 = 'AUTOUGH2_42D'
+
+model_dir = './run'
+orig_dir = os.getcwd()
+if not os.path.isdir(model_dir): os.makedirs(model_dir)
+os.chdir(model_dir)
+
+hr = 60. * 60.
+day = 24. * hr
+yr = 365.25 * day
+
+model_name = 'doublet'
+dimensions = [200., 10., 10.]
+nblks = [100, 1, 1]
+gridsizes = [dim / nblk for dim, nblk in zip(dimensions, nblks)]
+dx = [[gridsize] * nblk for gridsize, nblk in zip(gridsizes, nblks)]
+
+geo = mulgrid().rectangular(dx[0], dx[1], dx[2], atmos_type = 2)
+geo.write('g'+ model_name +'.dat')
+
+dat = t2data()
+dat.title = '1-D tracer doublet problem'
+dat.simulator = 'AUTOUGH2.2'
+
+dat.grid = t2grid().fromgeo(geo)
+rock = dat.grid.rocktypelist[0]
+rock.porosity = 0.1
+rock.density = 2600.
+rock.permeability = np.array([2.e-13, 2.e-15, 2.e-15])
+rock.conductivity = 1.5
+rock.specific_heat = 900.
+
+dat.relative_permeability = {'type': 1,
+                             'parameters': [0.5, 0., 1., 0.5, 0., 0., 0.]}
+dat.capillarity = {'type': 1,
+                   'parameters': [0., 0., 1., 0., 0., 0., 0.]}
+
+dat.lineq = {'type': 1, 'epsilon': 1.e-11}
+
+ndt = 500
+P0, T0 = 50.e5, 10.
+dat.parameter.update(
+    {'max_timesteps': ndt,
+     'tstop': 1.e15,
+     'print_interval': ndt,
+     'print_level': 1,
+     'default_incons': [P0, T0],
+     'const_timestep': 1.e4
+     })
+
+dat.parameter['option'][11] = 2 # permeability weighting
+dat.parameter['option'][12] = 2 # generation table interpolation
+dat.parameter['option'][16] = 5 # time step control
+dat.parameter['option'][24] = 0 # initial output
+
+dat.multi = {'eos': 'EW', 'num_components': 1, 'num_phases': 2,
+             'num_equations': 2, 'num_secondary_parameters': 6}
+
+lay = geo.layerlist[-1]
+
+# injection:
+q = 0.5
+h = 4.21e5
+col = geo.columnlist[0]
+blkname = geo.block_name(lay.name, col.name)
+gen = t2generator(name = 'inj 1', block = blkname,
+                  type = 'MASS', gx = q, ex = h)
+dat.add_generator(gen)
+
+# production:
+PI = 4.e-11
+Pc = 50.e5
+qmax = 5.0
+col = geo.columnlist[-1]
+blkname = geo.block_name(lay.name, col.name)
+gen = t2generator(name = 'prd 1', block = blkname,
+                  type = 'DELT', gx = PI, ex = Pc, hg = qmax)
+dat.add_generator(gen)
+
+inc = dat.grid.incons([P0, T0])
+
+mesh_filename = 'g' + model_name + '.msh'
+geo.write_mesh(mesh_filename, dimension = 2, slice = 'x',
+               file_format = 'gmsh2-binary')
+
+jsondata = dat.json(geo, mesh_filename, incons = inc, mesh_coords = 'xz')
+jsondata['mesh']['thickness'] = dimensions[1]
+jsondata['mesh']['zones'] = {"all": {"type": "box"}}
+del jsondata['rock']['types'][0]['cells']
+jsondata['rock']['types'][0]['zones'] = 'all'
+del jsondata['output']['filename']
+jsondata['output']['initial'] = False
+jsondata['logfile'] = {'echo': False}
+json.dump(jsondata, file(model_name + '_ss.json', 'w'), indent = 2)
+
+# add tracer to TOUGH2 model:
+dat.simulator = 'AUTOUGH2.2EWT'
+tracer_incons = [P0, T0, 0]
+dat.multi = {'eos': 'EWT', 'num_components': 2, 'num_phases': 2,
+             'num_equations': 3, 'num_secondary_parameters': 6}
+dat.parameter.update({
+    'default_incons': tracer_incons})
+dat.write(model_name + '_ss.dat')
+
+inc = dat.grid.incons(tracer_incons)
+inc.write(model_name + '_ss.incon')
+
+dat.run(simulator = AUTOUGH2,
+        incon_filename = model_name + '_ss.incon',
+        silent = True)
+t2incon(model_name + '_ss.save').write(model_name + '.incon')
+
+subprocess.call(['waiwera', model_name + '_ss.json'])
+
+# transient model:
+ndt = 300
+dat.parameter.update(
+    {'max_timesteps': ndt,
+     'tstop': 0.5 * yr,
+     'print_interval': 10,
+     'const_timestep': 0.01 * day,
+     'max_timestep': 1 * day,
+     })
+
+inflow_mass_fraction = 1.e-3
+tq = 0.15 * day
+qt = q * inflow_mass_fraction
+col = geo.columnlist[0]
+blkname = geo.block_name(lay.name, col.name)
+gen = t2generator(name = 'trc 1', block = blkname,
+                  type = 'COM2')
+gen.ltab = 4
+gen.itab = '1'
+gen.time = [0., tq, tq + 1.e-3, 1. * yr]
+gen.rate = [qt, qt, 0., 0.]
+gen.enthalpy = [h] * 4
+dat.add_generator(gen)
+
+dat.write(model_name + '.dat')
+
+dat.run(simulator = AUTOUGH2,
+        incon_filename = model_name + '.incon',
+        silent = True)
+
+jsondata['output']['frequency'] = dat.parameter['print_interval']
+jsondata['time']['step']['size'] = dat.parameter['const_timestep']
+jsondata['time']['stop'] = dat.parameter['tstop']
+jsondata['time']['step']['maximum']['number'] = ndt
+jsondata['time']['step']['maximum']['size'] = dat.parameter['max_timestep']
+
+jsondata['tracer'] = {"name": "1"}
+jsondata['source'][0]['tracer'] = [[0, inflow_mass_fraction], [tq, 0]]
+jsondata['source'][0]['interpolation'] = 'step'
+
+jsondata['initial'] = {'filename': model_name + '_ss.h5'}
+jsondata['logfile'] = {'echo': True}
+
+json.dump(jsondata, file(model_name + '.json', 'w'), indent = 2)
+
+os.chdir(orig_dir)
---
+++ test/benchmark/tracer/doublet/test_doublet.py
@@ -0,0 +1,136 @@
+"""
+1-D single-phase doublet problem with tracer injection and production
+"""
+
+import argparse
+import os
+import sys
+
+import matplotlib
+matplotlib.use('Agg')
+
+from credo.systest import SciBenchmarkTest
+
+from credo.jobrunner import SimpleJobRunner
+from credo.t2model import T2ModelRun, T2ModelResult
+from credo.waiwera import WaiweraModelRun
+from credo.modelresult import DigitisedOneDFieldResult
+
+import credo.reporting.standardReports as sReps
+from credo.reporting import getGenerators
+
+from credo.systest import FieldWithinTolTC
+
+from mulgrids import mulgrid
+
+import matplotlib.pyplot as plt
+
+from matplotlib import rcParams
+rcParams['mathtext.default'] = 'regular'
+
+day = 24. * 60. * 60.
+import numpy as np
+from docutils.core import publish_file
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-np", type = int, default = 1, help = "number of processes")
+parser.add_argument("-d", "--docker", action = "store_true",
+                    help = "run via Docker (waiwera-dkr)")
+args = parser.parse_args()
+mpi = args.np > 1 and not args.docker
+simulator = 'waiwera-dkr -np %d' % args.np if args.docker else 'waiwera'
+
+model_name = 'doublet'
+    
+AUTOUGH2_FIELDMAP = {
+    'Pressure': 'Pressure',
+    'Tracer mass fraction': 'Tracer/liquid'}
+
+WAIWERA_FIELDMAP = {
+    'Pressure': 'fluid_pressure',
+    'Tracer mass fraction': 'tracer_1'}
+
+model_dir = './run'
+data_dir = './data'
+t2geo_filename = os.path.join(model_dir, 'g' + model_name + '.dat')
+geo = mulgrid(t2geo_filename)
+map_out_bdy = list(range(0, geo.num_blocks))
+
+run_index = 0
+run_name = 'run'
+run_base_name = model_name
+output_indices = [0, 1, 2, 3, 6, 8]
+
+test_fields = ['Tracer mass fraction']
+field_unit = {'Tracer mass fraction': '10$^{-6}$'}
+field_scale = {'Tracer mass fraction': 1.e-6}
+
+doublet_test = SciBenchmarkTest(model_name + "_test", nproc = args.np)
+doublet_test.description = """1-D single-phase doublet problem with tracer injection
+and production
+"""
+
+run_filename = run_base_name + '.json'
+model_run = WaiweraModelRun(run_name, run_filename,
+                            fieldname_map = WAIWERA_FIELDMAP,
+                            simulator = simulator,
+                            basePath = os.path.realpath(model_dir))
+model_run.jobParams['nproc'] = args.np
+doublet_test.mSuite.addRun(model_run, run_name)
+
+doublet_test.setupEmptyTestCompsList()
+
+results_filename = os.path.join(model_dir, run_base_name + ".listing")
+AUTOUGH2_result = T2ModelResult("AUTOUGH2", results_filename,
+                                geo_filename = t2geo_filename,
+                                fieldname_map = AUTOUGH2_FIELDMAP,
+                                ordering_map = map_out_bdy)
+for output_index in output_indices:
+    doublet_test.addTestComp(run_index, "AUTOUGH2",
+                             FieldWithinTolTC(fieldsToTest = test_fields,
+                                              defFieldTol = 1.e-4,
+                                              expected = AUTOUGH2_result,
+                                              testOutputIndex = output_index))
+
+jrunner = SimpleJobRunner(mpi = mpi)
+testResult, mResults = doublet_test.runTest(jrunner, createReports = True)
+
+result = doublet_test.mSuite.resultsList[run_index]
+t = result.getTimes()
+x = np.array([col.centre[0] for col in geo.columnlist])
+
+for field_name in test_fields:
+    for output_index in output_indices:
+        output_str = str(output_index)
+        var = result.getFieldAtOutputIndex(field_name,
+                                           output_index) / field_scale[field_name]
+        plt.plot(x, var, 'b-', label = 'Waiwera', zorder = 2)
+        var = AUTOUGH2_result.getFieldAtOutputIndex(field_name,
+                                                    output_index) / field_scale[field_name]
+        plt.plot(x, var, 'gs', label = 'AUTOUGH2', zorder = 1)
+        plt.xlabel('x (m)')
+        plt.ylabel('%s (%s)' % (field_name, field_unit[field_name]))
+        img_filename_base = '_'.join((model_name, run_name, field_name, output_str))
+        img_filename_base = img_filename_base.replace(' ', '_')
+        img_filename = os.path.join(doublet_test.mSuite.runs[run_index].basePath,
+                                    doublet_test.mSuite.outputPathBase,
+                                    img_filename_base)
+        plt.legend(loc = 'best')
+        plt.title('%s at time %2.0f days' % (field_name, t[output_index] / day))
+        plt.tight_layout(pad = 3.)
+        plt.savefig(img_filename + '.png', dpi = 300)
+        plt.savefig(img_filename + '.pdf')
+        plt.clf()
+        doublet_test.mSuite.analysisImages.append(img_filename + '.png')
+
+# generate report:
+
+for rGen in getGenerators(["RST"], doublet_test.outputPathBase):
+    report_filename = os.path.join(doublet_test.outputPathBase,
+                     "%s-report.%s" % (doublet_test.testName, rGen.stdExt))
+    sReps.makeSciBenchReport(doublet_test, mResults, rGen, report_filename)
+    html_filename = os.path.join(doublet_test.outputPathBase,
+                     "%s-report.%s" % (doublet_test.testName, 'html'))
+    html = publish_file(source_path = report_filename,
+                        destination_path = html_filename,
+                        writer_name = "html")
