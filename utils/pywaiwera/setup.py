55d26483b3392b70985fb25f998fbda3297c6154
Adrian Croucher <a.croucher@auckland.ac.nz>
2022-06-29 11:27:42 +1200

    Version 1.3.1

--- utils/pywaiwera/setup.py
+++ utils/pywaiwera/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pywaiwera",
-    version="1.3.0",
+    version="1.3.1",
     author="Waiwera Project",
     author_email="waiwera.project@gmail.com",
     description="Python package for the Waiwera simulator",
