92f2e12a3a9c1db6cb196e2d5db9643d27f667b8
Adrian Croucher <a.croucher@auckland.ac.nz>
2022-10-28 14:31:26 +1300

    Add halite_solubility_two_phase()

--- src/salt_thermodynamics.F90
+++ src/salt_thermodynamics.F90
@@ -6,20 +6,22 @@ module salt_thermodynamics_module
   use petscsys
   use kinds_module
   use thermodynamics_module
-  use utils_module, only: polynomial
+  use utils_module, only: polynomial, newton1d
 
   implicit none
   private
   PetscReal, parameter :: salt_molecular_weight = 58.448_dp ! g/mol
   PetscReal, parameter :: halite_solubility_data(3) = &
        [2.6218e-1_dp, 7.2e-2_dp, 1.06_dp]
+  PetscReal, parameter :: halite_solubility_two_phase_data(5) = &
+       [0.2876823_dp, 0.30122157_dp, -0.39877656_dp, 0.31352381_dp, -0.09062578_dp]
   PetscReal, parameter :: brine_sat_pressure_a_data(4) = &
        [0._dp, 5.93582e-1_dp, -5.19386_dp, 1.23156_dp]
   PetscReal, parameter :: brine_sat_pressure_b_data(6) = &
        [0._dp, 1.15420_dp, 1.41254_dp, -1.92476_dp, &
        -1.70717_dp, 1.05390_dp]
 
-  public :: halite_solubility
+  public :: halite_solubility, halite_solubility_two_phase
   public :: brine_saturation_pressure, brine_saturation_temperature
 
 contains
@@ -43,6 +45,49 @@ contains
     end if
 
   end subroutine halite_solubility
+
+!------------------------------------------------------------------------
+
+  subroutine halite_solubility_two_phase(pressure, thermo, solubility, err)
+    !! Returns solubility of salt in liquid water on the brine
+    !! saturation line, given the total pressure.
+
+    PetscReal, intent(in) :: pressure
+    class(thermodynamics_type), intent(in out) :: thermo !! Water thermodynamics
+    PetscReal, intent(out) :: solubility
+    PetscErrorCode, intent(out) :: err
+    ! Locals:
+    PetscReal :: xs
+    PetscInt, parameter :: maxit = 100
+    PetscReal, parameter :: tol = 1.e-10_dp
+    PetscReal, parameter :: inc = 1.e-8_dp
+
+    ! initial estimate from polynomial fit:
+    xs = polynomial(halite_solubility_two_phase_data, pressure / 1.e7_dp)
+
+    call newton1d(f, xs, inc, tol, maxit, err)
+    solubility = xs
+
+  contains
+
+    PetscReal function f(x, err)
+      PetscReal, intent(in) :: x
+      PetscErrorCode, intent(out) :: err
+      ! Locals:
+      PetscReal :: temperature, solubility
+
+      call brine_saturation_temperature(pressure, x, thermo, &
+           temperature, err)
+      if (err == 0) then
+         call halite_solubility(temperature, solubility, err)
+         f = x - solubility
+      else
+         f = -1._dp
+      end if
+
+    end function f
+
+  end subroutine halite_solubility_two_phase
 
 !------------------------------------------------------------------------
 
@@ -79,8 +124,6 @@ contains
        thermo, saturation_temperature, err)
     !! Saturation temperature of brine at given pressure and salt mass
     !! fraction, using the specified water thermodynamics.
-
-    use utils_module, only: newton1d
 
     PetscReal, intent(in) :: pressure !! Pressure (Pa)
     PetscReal, intent(in) :: salt_mass_fraction !! Salt mass fraction
