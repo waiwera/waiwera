66dba858351abb98acd55932870c12fe3f034095
Adrian Croucher <a.croucher@auckland.ac.nz>
2024-02-21 16:17:45 +1300

    Remove Sphinx theme sidebar config

--- doc/user/conf.py
+++ doc/user/conf.py
@@ -182,13 +182,7 @@ html_static_path = ['_static']
 
 # Custom sidebar templates, maps document names to template names.
 #
-html_sidebars = {
-    '**': [
-        'localtoc.html',
-        'relations.html',  # needs 'show_related': True theme option to display
-        'searchbox.html',
-    ]
-}
+# html_sidebars = {}
 
 # Additional templates that should be rendered to pages, maps page names to
 # template names.
