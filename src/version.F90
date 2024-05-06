0c396f5ebab1c8c4e23f52047bfeed648df8b42a
Adrian Croucher <a.croucher@auckland.ac.nz>
2024-05-06 15:59:59 +1200

    Version 1.5.0b1

--- src/version.F90
+++ src/version.F90
@@ -27,6 +27,6 @@ module version_module
   
   PetscInt, parameter :: max_version_string_length = 64
   character(len = max_version_string_length), public :: &
-       waiwera_version = "1.4.0"
+       waiwera_version = "1.5.0b1"
 
 end module version_module
