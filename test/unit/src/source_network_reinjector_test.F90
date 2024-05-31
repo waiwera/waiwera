5fa524af6be826377a1db01c35b8a04ea4ecbf3d
Adrian Croucher <a.croucher@auckland.ac.nz>
2024-05-31 15:17:18 +1200

    Add source network dependencies unit tests [ci skip]

--- test/unit/src/source_network_reinjector_test.F90
+++ test/unit/src/source_network_reinjector_test.F90
@@ -22,7 +22,7 @@ module source_network_reinjector_test
   character(len = 512) :: data_path
 
   public :: setup, teardown
-  public :: test_source_network_reinjector
+  public :: test_source_network_reinjector, test_source_network_deps
 
 contains
 
@@ -547,6 +547,144 @@ contains
 
 !------------------------------------------------------------------------
 
+  subroutine test_source_network_deps(test)
+    ! Source network dependencies
+
+    use fson
+    use fson_mpi_module
+    use IAPWS_module
+    use eos_we_module
+    use mesh_module
+    use tracer_module
+    use dm_utils_module, only: global_vec_section, global_section_offset
+
+    class(unit_test_type), intent(in out) :: test
+    ! Locals:
+    type(IAPWS_type) :: thermo
+    type(eos_we_type) :: eos
+    type(fson_value), pointer :: json
+    type(mesh_type) :: mesh
+    type(tracer_type), allocatable :: tracers(:)
+    Vec :: fluid_vector
+    PetscInt :: fluid_range_start
+    PetscReal, pointer, contiguous :: source_array(:), group_array(:), reinjector_array(:)
+    PetscSection :: source_section, group_section, reinjector_section
+    type(source_network_type) :: source_network
+    PetscMPIInt :: rank
+    PetscReal, parameter :: start_time = 100._dp, end_time = 200._dp
+    PetscReal, parameter :: interval(2) = [start_time, end_time]
+    PetscReal, parameter :: gravity(3) = [0._dp, 0._dp, -9.8_dp]
+    PetscErrorCode :: err, ierr
+    PetscInt, parameter :: expected_num_sources = 9
+    PetscInt, parameter :: expected_num_groups = 2
+    PetscInt, parameter :: expected_num_reinjectors = 2
+    PetscInt, parameter :: expected_num_deps = 10
+    PetscInt, parameter :: expected_deps(expected_num_deps, 2) = transpose(reshape( &
+         [42, 0,  2, 4,  4, 2,  45, 6,  48, 6, 45, 17,  48, 17, 31, 6, 31, 17, 48, 45], &
+         [2, expected_num_deps]))
+    PetscBool, parameter :: update_reinjection = PETSC_TRUE
+
+    call MPI_COMM_RANK(PETSC_COMM_WORLD, rank, ierr)
+    json => fson_parse_mpi(trim(adjustl(data_path)) // "source/test_source_network_deps.json")
+
+    call thermo%init()
+    call eos%init(json, thermo)
+    call setup_tracers(json, eos, tracers, err = err)
+    call mesh%init(eos, json)
+    call DMCreateLabel(mesh%serial_dm, open_boundary_label_name, ierr); CHKERRQ(ierr)
+    call mesh%configure(gravity, json, err = err)
+    call DMGetGlobalVector(mesh%dm, fluid_vector, ierr); CHKERRQ(ierr) ! dummy- not used
+
+    call setup_source_network(json, mesh%dm, mesh%cell_natural_global, eos, tracers, &
+         thermo, start_time, fluid_vector, fluid_range_start, source_network, &
+         err = err)
+
+    call test%assert(0, err, "error")
+    if (rank == 0) then
+       call test%assert(expected_num_sources, source_network%num_sources, "number of sources")
+       call test%assert(expected_num_groups, source_network%num_groups, "number of groups")
+       call test%assert(expected_num_reinjectors, source_network%num_reinjectors, &
+            "number of reinjectors")
+    end if
+
+    if (err == 0) then
+
+       call global_vec_section(source_network%source, source_section)
+       call global_vec_section(source_network%group, group_section)
+       call global_vec_section(source_network%reinjector, reinjector_section)
+       call VecGetArrayF90(source_network%source, source_array, ierr); CHKERRQ(ierr)
+       call VecGetArrayF90(source_network%group, group_array, ierr); CHKERRQ(ierr)
+       call VecGetArrayF90(source_network%reinjector, reinjector_array, ierr); CHKERRQ(ierr)
+
+       call source_network%groups%traverse(group_assign_iterator)
+       call source_network%reinjectors%traverse(reinjector_assign_iterator)
+
+       call VecRestoreArrayF90(source_network%reinjector, reinjector_array, ierr); CHKERRQ(ierr)
+       call VecRestoreArrayF90(source_network%group, group_array, ierr); CHKERRQ(ierr)
+       call VecRestoreArrayF90(source_network%source, source_array, ierr); CHKERRQ(ierr)
+
+       call source_network_dependency_test(test, source_network, expected_deps, rank)
+
+    end if
+
+    call source_network%destroy()
+    call DMRestoreGlobalVector(mesh%dm, fluid_vector, ierr); CHKERRQ(ierr)
+    call mesh%destroy_distribution_data()
+    call mesh%destroy()
+    call eos%destroy()
+    call thermo%destroy()
+    call fson_destroy_mpi(json)
+
+  contains
+
+!........................................................................
+
+    subroutine group_assign_iterator(node, stopped)
+
+      type(list_node_type), pointer, intent(in out) :: node
+      PetscBool, intent(out) :: stopped
+      ! Locals:
+      PetscInt :: g, group_offset
+
+      stopped = PETSC_FALSE
+      select type(group => node%data)
+      class is (source_network_group_type)
+         if (group%rank == 0) then
+            g = group%local_group_index
+            group_offset = global_section_offset(group_section, &
+              g, source_network%group_range_start)
+            call group%assign(group_array, group_offset)
+         end if
+      end select
+
+    end subroutine group_assign_iterator
+
+!........................................................................
+
+    subroutine reinjector_assign_iterator(node, stopped)
+
+      type(list_node_type), pointer, intent(in out) :: node
+      PetscBool, intent(out) :: stopped
+      ! Locals:
+      PetscInt :: r, reinjector_offset
+
+      stopped = PETSC_FALSE
+      select type(reinjector => node%data)
+      class is (source_network_reinjector_type)
+         if (reinjector%rank == 0) then
+            r = reinjector%local_reinjector_index
+            reinjector_offset = global_section_offset(reinjector_section, &
+              r, source_network%reinjector_range_start)
+            call reinjector%assign(reinjector_array, reinjector_offset)
+         end if
+      end select
+
+    end subroutine reinjector_assign_iterator
+
+  end subroutine test_source_network_deps
+
+!------------------------------------------------------------------------
+
   subroutine source_network_dependency_test(test, source_network, &
        expected_deps, rank)
 
@@ -555,13 +693,16 @@ contains
     PetscInt, intent(in) :: expected_deps(:,:)
     PetscMPIInt, intent(in) :: rank
     ! Locals:
-    PetscInt, parameter :: max_num_cells = 20
-    PetscInt, parameter :: n2 = max_num_cells * max_num_cells
-    PetscBool :: nonzero(max_num_cells, max_num_cells)
-    PetscBool :: nonzero_exp(max_num_cells, max_num_cells)
+    PetscInt :: max_num_cells, n2
+    PetscBool, allocatable :: nonzero(:, :), nonzero_exp(:, :)
     PetscBool, allocatable :: nonzero1(:), nonzero1_all(:)
     PetscInt :: i
     PetscErrorCode :: ierr
+
+    max_num_cells = maxval(expected_deps) + 1
+    allocate(nonzero(0: max_num_cells - 1, 0: max_num_cells - 1))
+    allocate(nonzero_exp(0: max_num_cells - 1, 0: max_num_cells - 1))
+    n2 = max_num_cells * max_num_cells
 
     nonzero = PETSC_FALSE
     call source_network%dependencies%traverse(dependency_iterator)
@@ -578,7 +719,7 @@ contains
        nonzero = reshape(nonzero1_all, [max_num_cells, max_num_cells])
        nonzero_exp = PETSC_FALSE
        do i = 1, size(expected_deps, 1)
-          nonzero_exp(expected_deps(i,1) + 1, expected_deps(i,2) + 1) = PETSC_TRUE
+          nonzero_exp(expected_deps(i,1), expected_deps(i,2)) = PETSC_TRUE
        end do
        call test%assert(nonzero_exp, nonzero, "source network dependencies")
     end if
@@ -593,7 +734,7 @@ contains
       stopped = PETSC_FALSE
       select type (dep => node%data)
       class is (source_dependency_type)
-         nonzero(dep%equation + 1, dep%cell + 1) = PETSC_TRUE
+         nonzero(dep%equation, dep%cell) = PETSC_TRUE
       end select
 
     end subroutine dependency_iterator
