af96bea807f33d595ae687a318f3a6a7d7d2765a
Adrian Croucher <a.croucher@auckland.ac.nz>
2025-06-24 15:46:11 +1200

    Add eos_wae fluid properties unit tests (Pg = 0)

---
+++ test/unit/src/eos_wae_test.F90
@@ -0,0 +1,176 @@
+module eos_wae_test_module
+
+  ! Tests for eos_wae module (non-isothermal water and air NCG
+  ! equation of state)
+
+#include <petsc/finclude/petsc.h>
+
+  use petsc
+  use kinds_module
+  use zofu
+  use fluid_module
+  use rock_module
+  use relative_permeability_module
+  use capillary_pressure_module
+  use IAPWS_module
+  use fson
+  use fson_mpi_module
+  use eos_wae_module
+
+  implicit none
+  private
+
+  public :: setup, teardown, setup_test
+  public :: test_eos_wae_fluid_properties
+
+contains
+
+!------------------------------------------------------------------------
+
+  subroutine setup()
+
+    use profiling_module, only: init_profiling
+
+    ! Locals:
+    PetscErrorCode :: ierr
+
+    call PetscInitialize(PETSC_NULL_CHARACTER, ierr); CHKERRQ(ierr)
+    call init_profiling()
+
+  end subroutine setup
+
+!------------------------------------------------------------------------
+
+  subroutine teardown()
+
+    PetscErrorCode :: ierr
+
+    call PetscFinalize(ierr); CHKERRQ(ierr)
+
+  end subroutine teardown
+
+!------------------------------------------------------------------------
+
+  subroutine setup_test(test)
+
+    class(unit_test_type), intent(in out) :: test
+
+    test%tolerance = 1.e-9
+
+  end subroutine setup_test
+
+!------------------------------------------------------------------------
+
+  subroutine test_eos_wae_fluid_properties(test)
+
+    ! eos_wae fluid properties test
+
+    class(unit_test_type), intent(in out) :: test
+    ! Locals:
+    type(fluid_type) :: fluid
+    type(rock_type) :: rock
+    PetscInt,  parameter :: offset = 1, num_primary_variables = 3
+    PetscReal, pointer, contiguous :: fluid_data(:)
+    PetscReal, allocatable :: expected(:)
+    PetscReal :: primary(num_primary_variables)
+    PetscInt :: region, expected_err
+    type(eos_wae_type) :: eos
+    type(IAPWS_type) :: thermo
+    class(relative_permeability_type), allocatable :: rp
+    class(capillary_pressure_type), allocatable :: cp
+    type(fson_value), pointer :: json
+    character(2) :: json_str = '{}'
+    character(60) :: title
+    PetscErrorCode :: err
+    PetscMPIInt :: rank
+    PetscInt :: ierr
+    PetscReal, parameter :: small = 1.e-6_dp
+
+    call MPI_COMM_RANK(PETSC_COMM_WORLD, rank, ierr)
+
+    json => fson_parse_mpi(str = json_str)
+    call thermo%init()
+    call eos%init(json, thermo)
+    call setup_relative_permeabilities(json, rp)
+    call setup_capillary_pressures(json, cp)
+    call rock%assign_relative_permeability(rp)
+    call rock%assign_capillary_pressure(cp)
+    call fluid%init(eos%num_components, eos%num_phases)
+    allocate(fluid_data(fluid%dof), expected(fluid%dof))
+    fluid_data = 0._dp
+    call fluid%assign(fluid_data, offset)
+    call rock%init()
+
+    if (rank == 0) then
+
+       title = 'cold water, Pg = 0'
+       primary = [1.e5_dp, 20._dp, 0._dp]
+       region = 1
+       expected = 0._dp
+       expected(1: 10) = [1.e5_dp, 20._dp, 1._dp, 0._dp, 1._dp, 1._dp, 1._dp, 0._dp, &
+            1.e5_dp, 0._dp]
+       expected(11: 19) = [998.2054863776967_dp, 0.0010015972622270245_dp, 1._dp, &
+            1._dp, 0._dp, 84011.81116713627_dp, 83911.6313931672_dp, 1._dp, 0._dp]
+       expected_err = 0
+       call properties_test(title, primary, region, expected, expected_err)
+
+       title = 'steam, Pg = 0'
+       primary = [1.e5_dp, 150._dp, 0._dp]
+       region = 2
+       expected = 0._dp
+       expected(1: 10) = [1.e5_dp, 150._dp, 2._dp, 0._dp, 2._dp, 1._dp, 0._dp, 0._dp, &
+            1.e5_dp, 0._dp]
+       expected(20: 28) = [0.5163351360139934_dp, 1.419241230472252e-5_dp, 1._dp, &
+            1._dp, 0._dp, 2776591.815449922_dp, 2582919.153984385_dp, 1._dp, 0._dp]
+       expected_err = 0
+       call properties_test(title, primary, region, expected, expected_err)
+
+       title = '2-phase, Pg = 0'
+       primary = [10.e5_dp, 0.4_dp, 0._dp]
+       region = 4
+       expected = 0._dp
+       expected(1: 10) = [10.e5_dp, 179.88563239146663_dp, 4._dp, 0._dp, 3._dp, &
+            1._dp, 0.6_dp, 0._dp, 10.e5_dp, 0._dp]
+       expected(11: 19) = [887.1274516747791_dp, 0.00015048492650911237_dp, 0.6_dp, &
+            0.6_dp, 0._dp, 762682.8443354106_dp, 761555.6105900089_dp, 1._dp, 0._dp]
+       expected(20: 28) = [5.145385853182684_dp, 1.4981316222701134e-5_dp, 0.4_dp, &
+            0.4_dp, 0._dp, 2777119.5376846623_dp, 2582770.65335727_dp, 1._dp, 0._dp]
+       expected_err = 0
+       call properties_test(title, primary, region, expected, expected_err)
+
+    end if
+
+    call rock%destroy()
+    call fluid%destroy()
+    deallocate(fluid_data, expected)
+    call eos%destroy()
+    call thermo%destroy()
+    call fson_destroy_mpi(json)
+    deallocate(rp, cp)
+
+  contains
+
+    subroutine properties_test(title, primary, region, expected, expected_err)
+
+      character(*), intent(in) :: title
+      PetscReal, intent(in) :: primary(:)
+      PetscInt, intent(in) :: region
+      PetscReal, intent(in) :: expected(:)
+      PetscInt, intent(in) :: expected_err
+
+      fluid%region = dble(region)
+      call eos%fluid_properties(primary, rock, fluid, err)
+
+      call test%assert(expected_err, err, trim(title) // ' err')
+      if (err == 0) then
+         call test%assert(expected, fluid_data(offset: offset + fluid%dof - 1), &
+              trim(title) // ' data')
+      end if
+
+    end subroutine properties_test
+
+  end subroutine test_eos_wae_fluid_properties
+
+!------------------------------------------------------------------------
+
+end module eos_wae_test_module
