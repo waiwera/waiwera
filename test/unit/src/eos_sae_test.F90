06012accf8706cbc80bfa2baf4b91a84507d73de
Adrian Croucher <a.croucher@auckland.ac.nz>
2025-09-05 17:45:13 +1200

    Add eos_sae boundary consistency unit tests

--- test/unit/src/eos_sae_test.F90
+++ test/unit/src/eos_sae_test.F90
@@ -16,12 +16,13 @@ module eos_sae_test_module
   use fson
   use fson_mpi_module
   use eos_sae_module
+  use unit_test_utils_module, only: fluid_compare
 
   implicit none
   private
 
   public :: setup, teardown, setup_test
-  public :: test_eos_sae_fluid_properties
+  public :: test_eos_sae_fluid_properties, test_eos_sae_bdy_consistency
 
 contains
 
@@ -350,6 +351,249 @@ contains
     end subroutine properties_test
 
   end subroutine test_eos_sae_fluid_properties
+
+!------------------------------------------------------------------------
+
+  subroutine test_eos_sae_bdy_consistency(test)
+
+    ! Test eos_sae boundary consistency
+
+    class(unit_test_type), intent(in out) :: test
+    ! Locals:
+    type(fson_value), pointer :: json
+    type(IAPWS_type) :: thermo
+    type(eos_sae_type) :: eos
+    PetscReal, pointer, contiguous :: fluid_data(:), rock_data(:)
+    type(fluid_type) :: fluid1, fluid2
+    type(rock_type) :: rock
+    class(relative_permeability_type), allocatable :: rp
+    class(capillary_pressure_type), allocatable :: cp
+    PetscMPIInt :: rank
+    PetscInt :: ierr
+    PetscReal, allocatable :: primary1(:), primary2(:)
+    PetscReal :: props(2), t, d, Pw1
+
+    PetscErrorCode :: err
+
+    call MPI_COMM_RANK(PETSC_COMM_WORLD, rank, ierr)
+
+    json => fson_parse_mpi(str = '{}')
+    call thermo%init()
+    call eos%init(json, thermo)
+    allocate(primary1(eos%num_primary_variables))
+    allocate(primary2(eos%num_primary_variables))
+    call fluid1%init(eos%num_components, eos%num_phases)
+    call fluid2%init(eos%num_components, eos%num_phases)
+    call rock%init()
+    allocate(fluid_data(fluid1%dof + fluid2%dof))
+    allocate(rock_data(rock%dof))
+    call setup_relative_permeabilities(json, rp)
+    call setup_capillary_pressures(json, cp)
+
+    fluid_data = 0._dp
+    rock_data = 0._dp
+
+    call fluid1%assign(fluid_data, 1)
+    call fluid2%assign(fluid_data, fluid1%dof + 1)
+    call rock%assign(rock_data, 1)
+    call rock%assign_relative_permeability(rp)
+    call rock%assign_capillary_pressure(cp)
+
+    if (rank == 0) then
+
+       ! region 1 / 3
+       associate (P1 => primary1(1), T1 => primary1(2), &
+            Pa1 => primary1(3), d2 => primary2(1), &
+            T2 => primary2(2), Pa2 => primary2(3), &
+            density => props(1))
+         P1 = 40.e6_dp
+         T1 = thermo%temperature_bdy_1_3
+         Pa1 = 1.e5_dp
+         call thermo%water%properties([P1 - Pa1, T1], props, err)
+         d2 = density
+         T2 = T1
+         Pa2 = Pa1
+       end associate
+       fluid1%region = dble(1)
+       fluid2%region = dble(3)
+       call eos%fluid_properties(primary1, rock, fluid1, err)
+       call eos%fluid_properties(primary2, rock, fluid2, err)
+       call fluid_compare(test, fluid1, fluid2, "region 1/3")
+
+       ! region 2 / 3
+       associate (P1 => primary1(1), T1 => primary1(2), &
+            Pa1 => primary1(3), d2 => primary2(1), &
+            T2 => primary2(2), Pa2 => primary2(3), &
+            density => props(1))
+         T1 = 400._dp
+         Pa1 = 2.e5_dp
+         call thermo%boundary23%pressure(T1, Pw1)
+         P1 = Pw1 + Pa1
+         call thermo%steam%properties([Pw1, T1], props, err)
+         d2 = density
+         T2 = T1
+         Pa2 = Pa1
+       end associate
+       fluid1%region = dble(2)
+       fluid2%region = dble(3)
+       call eos%fluid_properties(primary1, rock, fluid1, err)
+       call eos%fluid_properties(primary2, rock, fluid2, err)
+       call fluid_compare(test, fluid1, fluid2, "region 2/3")
+
+       ! region 4 at 350 deg C
+       associate (P1 => primary1(1), Sv1 => primary1(2), &
+            Pa1 => primary1(3), P2 => primary2(1), &
+            Sv2 => primary2(2), Pa2 => primary2(3))
+         Pw1 = thermo%saturation_pressure_bdy_1_3
+         Pa1 = 5.e5_dp
+         P1 = Pw1 + Pa1
+         Sv1 = 0.5_dp
+         P2 = P1 + 1.e-9_dp
+         Sv2 = Sv1
+         Pa2 = Pa1
+       end associate
+       fluid1%region = dble(4)
+       fluid2%region = dble(4)
+       call eos%fluid_properties(primary1, rock, fluid1, err)
+       call eos%fluid_properties(primary2, rock, fluid2, err)
+       call fluid_compare(test, fluid1, fluid2, "region 4 350 C")
+
+       ! region 4 / 3 liquid
+       associate (P1 => primary1(1), Sv1 => primary1(2), &
+            Pa1 => primary1(3), d2 => primary2(1), &
+            T2 => primary2(2), Pa2 => primary2(3))
+         Pw1 = 18.e6_dp
+         call thermo%saturation%temperature(Pw1, t, err)
+         Pa1 = 7.e5_dp
+         P1 = Pw1 + Pa1
+         Sv1 = 0._dp
+         select type (region => thermo%supercritical)
+         type is (IAPWS_region3_type)
+            call region%saturation_density([Pw1, t], &
+                 PETSC_TRUE, d, err, PETSC_TRUE)
+         end select
+         d2 = d + 1.e-9_dp
+         T2 = t
+         Pa2 = Pa1
+       end associate
+       fluid1%region = dble(4)
+       fluid2%region = dble(3)
+       call eos%fluid_properties(primary1, rock, fluid1, err)
+       call eos%fluid_properties(primary2, rock, fluid2, err)
+       call fluid_compare(test, fluid1, fluid2, "region 4/3 L")
+
+       ! region 4 / 3 liquid, near-critical
+       associate (P1 => primary1(1), Sv1 => primary1(2), &
+            Pa1 => primary1(3), d2 => primary2(1), &
+            T2 => primary2(2), Pa2 => primary2(3))
+         Pw1 = 21.99e6_dp
+         call thermo%saturation%temperature(Pw1, t, err)
+         Pa1 = 2.e5_dp
+         P1 = Pw1 + Pa1
+         Sv1 = 0._dp
+         select type (region => thermo%supercritical)
+         type is (IAPWS_region3_type)
+            call region%saturation_density([Pw1, t], &
+                 PETSC_TRUE, d, err, PETSC_TRUE)
+         end select
+         d2 = d + 1.e-6_dp
+         T2 = t
+         Pa2 = Pa1
+       end associate
+       fluid1%region = dble(4)
+       fluid2%region = dble(3)
+       call eos%fluid_properties(primary1, rock, fluid1, err)
+       call eos%fluid_properties(primary2, rock, fluid2, err)
+       call fluid_compare(test, fluid1, fluid2, &
+            "region 4/3 near-critical L", tol = 1.e-8_dp)
+
+       ! region 4 / 3 vapour
+       associate (P1 => primary1(1), Sv1 => primary1(2), &
+            Pa1 => primary1(3), d2 => primary2(1), &
+            T2 => primary2(2), Pa2 => primary2(3))
+         Pw1 = 18.e6_dp
+         Sv1 = 1._dp
+         Pa1 = 1.5e5_dp
+         P1 = Pw1 + Pa1
+         call thermo%saturation%temperature(Pw1, t, err)
+         select type (region => thermo%supercritical)
+         type is (IAPWS_region3_type)
+            call region%saturation_density([Pw1, t], &
+                 PETSC_FALSE, d, err, PETSC_TRUE)
+         end select
+         d2 = d - 1.e-9_dp
+         T2 = t
+         Pa2 = Pa1
+       end associate
+       fluid1%region = dble(4)
+       fluid2%region = dble(3)
+       call eos%fluid_properties(primary1, rock, fluid1, err)
+       call eos%fluid_properties(primary2, rock, fluid2, err)
+       call fluid_compare(test, fluid1, fluid2, "region 4/3 V")
+
+       ! region 4 / 3 vapour, near-critical
+       associate (P1 => primary1(1), Sv1 => primary1(2), &
+            Pa1 => primary1(3), d2 => primary2(1), &
+            T2 => primary2(2), Pa2 => primary2(3))
+         Pw1 = 21.99e6_dp
+         Sv1 = 1._dp
+         Pa1 = 1.3e5_dp
+         P1 = Pw1 + Pa1
+         call thermo%saturation%temperature(Pw1, t, err)
+         select type (region => thermo%supercritical)
+         type is (IAPWS_region3_type)
+            call region%saturation_density([Pw1, t], &
+                 PETSC_FALSE, d, err, PETSC_TRUE)
+         end select
+         d2 = d
+         T2 = t + 1.e-9_dp
+         Pa2 = Pa1
+       end associate
+       fluid1%region = dble(4)
+       fluid2%region = dble(3)
+       call eos%fluid_properties(primary1, rock, fluid1, err)
+       call eos%fluid_properties(primary2, rock, fluid2, err)
+       call fluid_compare(test, fluid1, fluid2, "region 4/3 near-critical V")
+
+       ! region 4 liquid / vapour at critical point
+       associate (P1 => primary1(1), Sv1 => primary1(2), &
+            Pa1 => primary1(3), P2 => primary2(1), &
+            Sv2 => primary2(2), Pa2 => primary2(3))
+         Pw1 = thermo%critical%pressure - 1.e-9_dp
+         Sv1 = 0._dp
+         Pa1 = 1.e5_dp
+         P1 = Pw1 + Pa1
+         P2 = P1
+         Sv2 = 1._dp
+         Pa2 = Pa1
+       end associate
+       fluid1%region = dble(4)
+       fluid2%region = dble(4)
+       call eos%fluid_properties(primary1, rock, fluid1, err)
+       call eos%fluid_properties(primary2, rock, fluid2, err)
+       call test%assert(fluid1%phase(1)%density, fluid2%phase(2)%density, &
+            "region 4 L/V CP density", tol = 1.e-10_dp)
+       call test%assert(fluid1%phase(1)%viscosity, fluid2%phase(2)%viscosity, &
+            "region 4 L/V CP viscosity", tol = 1.e-10_dp)
+       call test%assert(fluid1%phase(1)%specific_enthalpy, &
+            fluid2%phase(2)%specific_enthalpy, &
+            "region 4 L/V CP enthalpy", tol = 1.e-10_dp)
+
+    end if
+
+    call fluid1%destroy()
+    call fluid2%destroy()
+    call rock%destroy()
+    call eos%destroy()
+    call thermo%destroy()
+    call fson_destroy_mpi(json)
+    deallocate(fluid_data, primary1, primary2)
+    call rp%destroy()
+    deallocate(rp)
+    call cp%destroy()
+    deallocate(cp)
+
+  end subroutine test_eos_sae_bdy_consistency
 
 !------------------------------------------------------------------------
 
