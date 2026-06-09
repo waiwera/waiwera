d30b4c8e6d9a18a117150e00b2944855e36aff05
Adrian Croucher <a.croucher@auckland.ac.nz>
2026-06-09 16:10:07 +1200

    Refactor IAPWS region1_properties() and modify interpolation at 350 C

--- src/IAPWS.F90
+++ src/IAPWS.F90
@@ -1686,66 +1686,39 @@ contains
     PetscReal, intent(out):: props(:) !! (density, internal energy)
     PetscInt, intent(out) :: err !! error code
     ! Locals:
-    PetscReal :: T_a, xi
-    PetscReal :: props_a(2), props_b(2), rho_b
+    PetscReal :: T_a, T_b
     PetscReal, parameter :: eps = 1.e-5_dp !! tolerance for differencing near max temperature
     PetscReal, parameter :: dT = 0.05_dp !! size of interpolation zone
 
     err = 0
-    select type (thermo => self%thermo)
-    type is (IAPWS_type)
-       associate (T_b => self%max_temperature, p => param(1), &
-            t => param(2))
+    associate (p => param(1), t => param(2))
 
-         if ((0._dp < t) .and. (t <= (1._dp + eps) * self%max_temperature) .and. &
-              (0._dp < p) .and. (p <= self%max_pressure)) then
+      if ((0._dp < t) .and. (t <= (1._dp + eps) * self%max_temperature) .and. &
+           (0._dp < p) .and. (p <= self%max_pressure)) then
 
+         select type (thermo => self%thermo)
+         type is (IAPWS_type)
             if (thermo%extrapolate) then
                call properties(param, props)
             else
-               if (p < thermo%saturation_pressure_bdy_1_3) then
+               T_b = self%max_temperature
+               T_a = T_b - dT
+               if (t < T_a) then
                   call properties(param, props)
                else
-
-                  T_a = T_b - dT
-
-                  if (t < T_a) then
-                     call properties(param, props)
-                  else
-
-                     call properties([p, T_a], props_a)
-                     select type (region3 => thermo%region(3)%ptr)
-                     type is (IAPWS_region3_type)
-                        call region3%density([p, T_b], rho_b, err, &
-                             polish = PETSC_TRUE, phases = SUBREGION_PHASES_LIQUID)
-                        if (err == 0) then
-                           call region3%properties([rho_b, T_b], props_b, err)
-                           if (err == 0) then
-                              associate (u_b => props_b(2))
-                                xi = (t - T_a) / dT
-                                props = (1._dp - xi) * props_a + xi * [rho_b, u_b]
-                              end associate
-                           end if
-                        end if
-                     end select
-
-                     if (err > 0) then ! fallback
-                        call properties(param, props)
-                        err = 0
-                     end if
-
-                  end if
-
+                  call interpolated_properties(param, T_a, T_b, props)
                end if
             end if
-         else
-            err = 1
-         end if
+         end select
+      else
+         err = 1
+      end if
 
-       end associate
-    end select
+    end associate
 
   contains
+
+!........................................................................
 
     subroutine properties(param, props)
 
@@ -1774,6 +1747,51 @@ contains
       end associate
 
     end subroutine properties
+
+!........................................................................
+
+    subroutine interpolated_properties(param, T_a, T_b, props)
+      !! Interpolates region 1 properties using linear
+      !! interpolation for temperatures within dT of the region 1/3
+      !! boundary.
+
+      PetscReal, intent(in) :: param(:) !! Primary variables (pressure, temperature)
+      PetscReal, intent(in) :: T_a, T_b !! Interpolation temperature range
+      PetscReal, intent(out):: props(:) !! (density, internal energy)
+
+      ! Locals:
+      PetscReal :: xi, P_b, rho_b
+      PetscReal :: props_a(2), props_b(2)
+
+      associate (p => param(1), t => param(2))
+        call properties([p, T_a], props_a)
+        if (err == 0) then
+           select type (thermo => self%thermo)
+           type is (IAPWS_type)
+              P_b = max(p, thermo%saturation_pressure_bdy_1_3)
+           end select
+           select type (region3 => self%thermo%region(3)%ptr)
+           type is (IAPWS_region3_type)
+              call region3%density([P_b, T_b], rho_b, err, &
+                   polish = PETSC_TRUE, phases = SUBREGION_PHASES_LIQUID)
+              if (err == 0) then
+                 call region3%properties([rho_b, T_b], props_b, err)
+                 if (err == 0) then
+                    props_b(1) = rho_b
+                    xi = (t - T_a) / dT
+                    props = (1._dp - xi) * props_a + xi * props_b
+                 end if
+              end if
+           end select
+        end if
+      end associate
+
+      if (err > 0) then ! fallback
+         call properties(param, props)
+         err = 0
+      end if
+
+    end subroutine interpolated_properties
 
   end subroutine region1_properties
 
