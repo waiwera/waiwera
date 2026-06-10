dfbcdfe052f33064478534fdc1c65b663e4498b8
Adrian Croucher <a.croucher@auckland.ac.nz>
2026-06-10 16:15:34 +1200

    Test logfile active separately in setup_deliverability_iterator()
    (otherwise source_control_test.F90 fails under debug)

--- src/source_setup.F90
+++ src/source_setup.F90
@@ -2963,10 +2963,12 @@ contains
              call deliv%calculate_PI_from_rate(start_time, initial_rate, &
                   fluid_data, fluid_section, fluid_range_start, &
                   deliv%productivity%val(1, 1))
-             if (present(logfile) .and. logfile%active) then
-                call logfile%write(LOG_LEVEL_INFO, 'input', 'calculated', &
-                     real_keys = [trim(srcstr) // "deliverability.productivity"], &
-                     real_values = [deliv%productivity%val(1, 1)])
+             if (present(logfile)) then
+                if (logfile%active) then
+                   call logfile%write(LOG_LEVEL_INFO, 'input', 'calculated', &
+                        real_keys = [trim(srcstr) // "deliverability.productivity"], &
+                        real_values = [deliv%productivity%val(1, 1)])
+                end if
              end if
           end if
           if (deliv%threshold > 0._dp) then
