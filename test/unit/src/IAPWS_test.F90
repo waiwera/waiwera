8e949ce3f0562e2d37c3d8da7be583e421b69be7
Adrian Croucher <a.croucher@auckland.ac.nz>
2026-06-15 14:03:34 +1200

    Update unit tests for _dp IAPWS parameters

--- test/unit/src/IAPWS_test.F90
+++ test/unit/src/IAPWS_test.F90
@@ -893,7 +893,7 @@ module IAPWS_test
        call pi_liquidlike_case(64.e6_dp, 520._dp, 0._dp, &
             0.2467814866882898_dp, 3, 0, 'case 5')
        call pi_liquidlike_case(64.e6_dp, 600._dp, 0._dp, &
-            1.9721624673232241e-2_dp, 3, 0, 'case 6')
+            0.01972162759518381_dp, 3, 0, 'case 6')
        call pi_liquidlike_case(40.e6_dp, 377._dp, 0._dp, &
             1.0_dp, 1, 0, 'case 7')
        call pi_liquidlike_case(30.e6_dp, 500._dp, 0._dp, &
