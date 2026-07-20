d48688dc681f0a17633b0bf3093d66e845395cda
Adrian Croucher <a.croucher@auckland.ac.nz>
2026-07-20 16:22:02 +1200

    Remove unused variables [ci skip]

--- src/IAPWS.F90
+++ src/IAPWS.F90
@@ -1394,7 +1394,7 @@ contains
     PetscInt, intent(out) :: pseudo_phases
     PetscErrorCode, intent(out) :: err
     ! Locals:
-    PetscReal :: widom_temperature, delta(2), xi, xit, xim
+    PetscReal :: widom_temperature, delta(2), xi, xit
     PetscReal, parameter :: eps = epsilon(pi_liq)
 
     err = 0
